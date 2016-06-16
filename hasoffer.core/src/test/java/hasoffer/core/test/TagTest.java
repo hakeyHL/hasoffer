92f4a2946ee1c6730105c1100f66e097cc56b92b
zhangchengwei <zhangchengwei@hasoffer.com>
2016-06-16 15:06:50 +0800

    创建tag库

--- hasoffer.core/src/test/java/hasoffer/core/test/TagTest.java
+++ hasoffer.core/src/test/java/hasoffer/core/test/TagTest.java
@@ -2,35 +2,88 @@ package hasoffer.core.test;
 
 import hasoffer.base.utils.StringUtils;
 import hasoffer.core.bo.match.HasTag;
+import hasoffer.core.persistence.dbm.osql.IDataBaseManager;
+import hasoffer.core.persistence.po.match.TagBrand;
+import hasoffer.core.persistence.po.match.TagCategory;
+import hasoffer.core.persistence.po.match.TagSkuVal;
 import jodd.io.FileUtil;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import javax.annotation.Resource;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Date : 2016/6/15
  * Function :
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:spring-beans.xml")
 public class TagTest {
 
+    @Resource
+    IDataBaseManager dbm;
+
     @Test
-    public void importTags() throws Exception {
-        String filePath = "d:/TMP/tags/catetag.txt";
+    public void importSkuValTags() throws Exception {
+        String filePath = "d:/TMP/tags/skuattrtag.txt";
 
         String[] lines = FileUtil.readLines(new File(filePath));
+
+        List<TagSkuVal> tags = new ArrayList<TagSkuVal>();
 
         for (String line : lines) {
             HasTag tag = cleanBySplit(line);
 
-            System.out.println(tag);
+            tags.add(new TagSkuVal(tag.getTag(), tag.getAlias(), 4));
         }
+
+        dbm.batchSave(tags);
+    }
+
+    @Test
+    public void importBrandTags() throws Exception {
+        String filePath = "d:/TMP/tags/brand.txt";
+
+        String[] lines = FileUtil.readLines(new File(filePath));
+
+        List<TagBrand> tags = new ArrayList<TagBrand>();
+
+        for (String line : lines) {
+            HasTag tag = cleanBySplit(line);
+
+            tags.add(new TagBrand(tag.getTag(), tag.getAlias(), 4));
+        }
+
+        dbm.batchSave(tags);
+    }
+
+    @Test
+    public void importCategoryTags() throws Exception {
+        String filePath = "d:/TMP/tags/catetag.txt";
+
+        String[] lines = FileUtil.readLines(new File(filePath));
+
+        List<TagCategory> tags = new ArrayList<TagCategory>();
+
+        for (String line : lines) {
+            HasTag tag = cleanBySplit(line);
+
+            tags.add(new TagCategory(tag.getTag(), tag.getAlias(), 10));
+        }
+
+        dbm.batchSave(tags);
     }
 
     private HasTag cleanBySplit(String line) {
 
         String[] tags = line.split(",");
 
-        String keyword = tags[0].trim();
+        String keyword = tags[0].trim().toLowerCase();
 
         HasTag ht = new HasTag();
         ht.setTag(keyword);
@@ -39,7 +92,7 @@ public class TagTest {
         if (len > 1) {
             StringBuffer sb = new StringBuffer();
             for (int i = 1; i < len; i++) {
-                String tag = tags[i].trim();
+                String tag = tags[i].trim().toLowerCase();
                 if (!StringUtils.isEmpty(tag)) {
                     sb.append(tag).append(",");
                 }
