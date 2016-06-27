ca867d62173b6be922e0371aae076707f7b65636
huli <hakeyhl@163.com>
2016-06-27 21:12:52 +0800

    去除用户id属性

--- hasoffer.api/src/main/java/hasoffer/api/controller/vo/UserVo.java
+++ hasoffer.api/src/main/java/hasoffer/api/controller/vo/UserVo.java
@@ -14,6 +14,7 @@ public class UserVo {
     private String Number;
     private  String name;
     private BigDecimal conis;
+
     public String getName() {
         return name;
     }
@@ -29,6 +30,7 @@ public class UserVo {
     public void setConis(BigDecimal conis) {
         this.conis = conis;
     }
+
     public String getThirdId() {
         return thirdId;
     }
