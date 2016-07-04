b4598648a62e09c681dce4ff02bfe4ae0a6ac1e8
wing-SF <18735117232@163.com>
2016-07-04 11:40:58 +0800

    sku visit update

--- hasoffer.task/src/main/java/hasoffer/task/worker/CmpSkuDubboUpdateWorker.java
+++ hasoffer.task/src/main/java/hasoffer/task/worker/CmpSkuDubboUpdateWorker.java
@@ -4,9 +4,10 @@ import hasoffer.base.exception.ContentParseException;
 import hasoffer.base.exception.HttpFetchException;
 import hasoffer.base.model.TaskStatus;
 import hasoffer.base.model.Website;
+import hasoffer.core.persistence.dbm.osql.IDataBaseManager;
 import hasoffer.core.persistence.po.ptm.PtmCmpSku;
+import hasoffer.core.persistence.po.search.SrmSearchLog;
 import hasoffer.core.product.ICmpSkuService;
-import hasoffer.core.worker.ListAndProcessWorkerStatus;
 import hasoffer.dubbo.api.fetch.service.IFetchDubboService;
 import hasoffer.fetch.helper.WebsiteHelper;
 import hasoffer.spider.model.FetchUrlResult;
@@ -14,6 +15,9 @@ import hasoffer.spider.model.FetchedProduct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -21,13 +25,16 @@ import java.util.concurrent.TimeUnit;
  */
 public class CmpSkuDubboUpdateWorker implements Runnable {
 
+    private static final String Q_PTMCMPSKU_BYPRODUCTID = "SELECT t FROM PtmCmpSku t WHERE t.productid = ?0 ";
     private static Logger logger = LoggerFactory.getLogger(CmpSkuDubboUpdateWorker.class);
-    private ListAndProcessWorkerStatus<PtmCmpSku> ws;
+    private IDataBaseManager dbm;
+    private ConcurrentLinkedQueue<SrmSearchLog> queue;
     private ICmpSkuService cmpSkuService;
     private IFetchDubboService fetchService;
 
-    public CmpSkuDubboUpdateWorker(ListAndProcessWorkerStatus<PtmCmpSku> ws, ICmpSkuService cmpSkuService, IFetchDubboService fetchService) {
-        this.ws = ws;
+    public CmpSkuDubboUpdateWorker(IDataBaseManager dbm, ConcurrentLinkedQueue<SrmSearchLog> queue, ICmpSkuService cmpSkuService, IFetchDubboService fetchService) {
+        this.dbm = dbm;
+        this.queue = queue;
         this.cmpSkuService = cmpSkuService;
         this.fetchService = fetchService;
     }
@@ -35,19 +42,15 @@ public class CmpSkuDubboUpdateWorker implements Runnable {
     @Override
     public void run() {
         while (true) {
-            PtmCmpSku sku = ws.getSdQueue().poll();
+            SrmSearchLog searchLog = queue.poll();
 
-            if (sku == null) {
+            if (searchLog == null) {
                 try {
                     TimeUnit.SECONDS.sleep(3);
                     logger.debug("task update get null sleep 3 seconds");
                 } catch (InterruptedException e) {
                     return;
                 }
-                continue;
-            }
-
-            if (!WebsiteHelper.DEFAULT_WEBSITES.contains(sku.getWebsite())) {
                 continue;
             }
 
@@ -59,60 +62,68 @@ public class CmpSkuDubboUpdateWorker implements Runnable {
 //                    }
 //                }
 
-            // try update sku
-            String url = sku.getUrl();
-            Website website = WebsiteHelper.getWebSite(url);
+            long productId = searchLog.getPtmProductId();
+            List<PtmCmpSku> skuList = dbm.query(Q_PTMCMPSKU_BYPRODUCTID, Arrays.asList(productId));
 
-            if (website == null) {
-                logger.debug(" parse website get null for [" + sku.getId() + "]");
-                continue;
+            for (PtmCmpSku sku : skuList) {
+                updatePtmCmpSku(sku, searchLog);
             }
+        }
+    }
 
-            FetchUrlResult fetchedResult = null;
+    private void updatePtmCmpSku(PtmCmpSku sku, SrmSearchLog searchLog) {
+        // try update sku
+        String url = sku.getUrl();
+        Website website = WebsiteHelper.getWebSite(url);
 
-            try {
-                fetchedResult = fetchService.getProductsByUrl(website, url);
-            } catch (HttpFetchException e) {
-                logger.debug("HttpFetchException for [" + sku.getId() + "]");
-            } catch (ContentParseException e) {
-                logger.debug("ContentParseException for [" + sku.getId() + "]");
-            }
+        if (website == null) {
+            logger.debug(" parse website get null for [" + sku.getId() + "]");
+            return;
+        }
 
-            TaskStatus taskStatus = fetchedResult.getTaskStatus();
+        FetchUrlResult fetchedResult = null;
 
-            FetchedProduct fetchedProduct = null;
+        try {
+            fetchedResult = fetchService.getProductsByUrl(website, url);
+        } catch (HttpFetchException e) {
+            logger.debug("HttpFetchException for [" + sku.getId() + "]");
+        } catch (ContentParseException e) {
+            logger.debug("ContentParseException for [" + sku.getId() + "]");
+        }
 
-            //如果返回结果状态为running，那么将sku返回队列
-            if (TaskStatus.RUNNING.equals(taskStatus) || TaskStatus.START.equals(taskStatus)) {
-                ws.getSdQueue().add(sku);
-                logger.debug("taskstatus RUNNING for [" + sku.getId() + "]");
-                continue;
-            } else if (TaskStatus.STOPPED.equals(taskStatus)) {
-                logger.debug("taskstatus STOPPED for [" + sku.getId() + "]");
-            } else if (TaskStatus.EXCEPTION.equals(taskStatus)) {
-                logger.debug("taskstatus EXCEPTION for [" + sku.getId() + "]");
-            } else {//(TaskStatus.FINISH.equals(taskStatus)))
-                logger.debug("taskstatus FINISH for [" + sku.getId() + "]");
-                fetchedProduct = fetchedResult.getFetchProduct();
-            }
+        TaskStatus taskStatus = fetchedResult.getTaskStatus();
 
+        FetchedProduct fetchedProduct = null;
 
-            //此处是FK、SD正常更新逻辑放弃对title字段的更新，该有另外的task统一维护
+        //如果返回结果状态为running，那么将sku返回队列
+        if (TaskStatus.RUNNING.equals(taskStatus) || TaskStatus.START.equals(taskStatus)) {
+            queue.add(searchLog);
+            logger.debug("taskstatus RUNNING for [" + sku.getId() + "]");
+            return;
+        } else if (TaskStatus.STOPPED.equals(taskStatus)) {
+            logger.debug("taskstatus STOPPED for [" + sku.getId() + "]");
+        } else if (TaskStatus.EXCEPTION.equals(taskStatus)) {
+            logger.debug("taskstatus EXCEPTION for [" + sku.getId() + "]");
+        } else {//(TaskStatus.FINISH.equals(taskStatus)))
+            logger.debug("taskstatus FINISH for [" + sku.getId() + "]");
+            fetchedProduct = fetchedResult.getFetchProduct();
+        }
+
+//        此处是FK、SD正常更新逻辑放弃对title字段的更新，该有另外的task统一维护
+//        切换新的更新模式，采用页面更新的方式，所有可以不用考虑title
+//        if (fetchedProduct != null) {
+//            if (Website.FLIPKART.equals(fetchedProduct.getWebsite()) || Website.SNAPDEAL.equals(fetchedProduct.getWebsite())) {
+//                fetchedProduct.setTitle(null);
+//            }
+//        }
+
+        try {
+            cmpSkuService.updateCmpSkuBySpiderFetchedProduct(sku.getId(), fetchedProduct);
+            logger.debug("fetch success for [" + sku.getId() + "]");
+        } catch (Exception e) {
+            logger.debug(e.toString());
             if (fetchedProduct != null) {
-                if (Website.FLIPKART.equals(fetchedProduct.getWebsite()) || Website.SNAPDEAL.equals(fetchedProduct.getWebsite())) {
-                    fetchedProduct.setTitle(null);
-                }
-            }
-
-            try {
-
-                cmpSkuService.updateCmpSkuBySpiderFetchedProduct(sku.getId(), fetchedProduct);
-                logger.debug("fetch success for [" + sku.getId() + "]");
-            } catch (Exception e) {
-                logger.debug(e.toString());
-                if (fetchedProduct != null) {
-                    logger.debug("title:" + fetchedProduct.getTitle());
-                }
+                logger.debug("title:" + fetchedProduct.getTitle());
             }
         }
     }
