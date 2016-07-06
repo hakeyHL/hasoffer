684eb7d7eebec2fba52c0c0f54b5a1e0c50687e5
wing-SF <18735117232@163.com>
2016-07-06 20:36:14 +0800

    top selling 后台编辑图片

--- hasoffer.admin/src/main/java/hasoffer/admin/controller/TopSellingController.java
+++ hasoffer.admin/src/main/java/hasoffer/admin/controller/TopSellingController.java
@@ -37,7 +37,7 @@ import java.util.Set;
 public class TopSellingController {
 
     private static final String CACHE_KEY_PRE = "PRODUCT_";
-    private static final String IMAGE_PREFIX = "img.hasoffer.com";
+    private static final String IMAGE_PREFIX = "http://img.hasoffer.com";
     @Resource
     ITopSellingService topSellingService;
     @Resource
