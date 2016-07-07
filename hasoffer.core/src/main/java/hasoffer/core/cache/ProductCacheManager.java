91975e684e13bb6580d7975160c1d87ab2bfdbc1
huli <hakeyhl@163.com>
2016-07-07 14:48:22 +0800

    过滤价格为0的商品

--- hasoffer.core/src/main/java/hasoffer/core/cache/ProductCacheManager.java
+++ hasoffer.core/src/main/java/hasoffer/core/cache/ProductCacheManager.java
@@ -163,7 +163,10 @@ public class ProductCacheManager {
                 for (SrmSearchCount srmSearchCount : srmSearchCounts) {
                     PageableResult<PtmCmpSku> pageableResult = productCacheManager.listPagedCmpSkus(srmSearchCount.getProductId(), 1, 20);
                     if (pageableResult != null && pageableResult.getData() != null && pageableResult.getData().size() > 0) {
-                        products.add(productService.getProduct(srmSearchCount.getProductId()));
+                        PtmProduct product = productService.getProduct(srmSearchCount.getProductId());
+                        if (product != null && product.getPrice() > 0) {
+                            products.add(productService.getProduct(srmSearchCount.getProductId()));
+                        }
                     }
                 }
                 if (products != null && products.size() > 0) {
