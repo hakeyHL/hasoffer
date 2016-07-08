7fbf6aa042f430b3c9271e52d7a6abf24891c92b
zhangchengwei <zhangchengwei@hasoffer.com>
2016-07-08 19:44:19 +0800

    保存每天的搜索记录

---
+++ hasoffer.timer/src/main/java/hasoffer/timer/controller/TimerController.java
@@ -0,0 +1,34 @@
+package hasoffer.timer.controller;
+
+import hasoffer.core.product.IProductService;
+import hasoffer.core.search.ISearchService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.annotation.Resource;
+
+/**
+ * Created on 2015/12/24.
+ */
+@Controller
+@RequestMapping(value = "/timer")
+public class TimerController {
+
+    @Resource
+    ISearchService searchService;
+    @Resource
+    IProductService productService;
+
+    @RequestMapping(value = "/statsearchlog/{ymd}", method = RequestMethod.GET)
+    public void f(@PathVariable String ymd) {
+
+        // 保存所有被搜索过的商品
+        searchService.saveSearchCount(ymd);
+
+        // top selling
+        productService.expTopSellingsFromSearchCount(ymd);
+    }
+
+}
