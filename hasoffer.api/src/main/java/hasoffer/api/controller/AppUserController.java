1bd4feecf029f3be4e3111c25a86bfd8b26a0435
huli <zjyjyhl@163.com>
2016-07-09 00:08:17 +0800

    修复:  对客户端上传的deepLink添加用户id 时 ,返回指定的数据结构

--- hasoffer.api/src/main/java/hasoffer/api/controller/AppUserController.java
+++ hasoffer.api/src/main/java/hasoffer/api/controller/AppUserController.java
@@ -11,8 +11,11 @@ import hasoffer.webcommon.context.StaticContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by hs on 2016/7/8.
@@ -24,7 +27,7 @@ public class AppUserController {
     AppServiceImpl appService;
 
     @RequestMapping("/addUserId2DeepLink")
-    public String get(@RequestParam String deepLink, String website) {
+    public ModelAndView get(@RequestParam String deepLink, String website) {
         String deviceId = (String) Context.currentContext().get(StaticContext.DEVICE_ID);
         DeviceInfoVo deviceInfo = (DeviceInfoVo) Context.currentContext().get(Context.DEVICE_INFO);
         SearchIO sio = new SearchIO("", "", "", website, "", deviceInfo.getMarketChannel(), deviceId, 0, 0);
@@ -36,6 +39,10 @@ public class AppUserController {
             affs = new String[]{sio.getMarketChannel().name(), sio.getDeviceId()};
         }
         String affsUrl = WebsiteHelper.getUrlWithAff(Website.valueOf(website), deepLink, affs);
-        return affsUrl;
+        ModelAndView modelAndView = new ModelAndView();
+        Map map = new HashMap();
+        map.put("deeplink", affsUrl);
+        modelAndView.addObject("data", map);
+        return modelAndView;
     }
 }
