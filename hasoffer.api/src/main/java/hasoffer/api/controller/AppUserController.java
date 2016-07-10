410866bb03ce81f44815535d23266d697630acab
huli <zjyjyhl@163.com>
2016-07-10 14:56:07 +0800

    若客户端usertoken未传送则返回原url且打印错误日志

--- hasoffer.api/src/main/java/hasoffer/api/controller/AppUserController.java
+++ hasoffer.api/src/main/java/hasoffer/api/controller/AppUserController.java
@@ -8,6 +8,8 @@ import hasoffer.core.system.impl.AppServiceImpl;
 import hasoffer.fetch.helper.WebsiteHelper;
 import hasoffer.webcommon.context.Context;
 import hasoffer.webcommon.context.StaticContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -23,16 +25,25 @@ import java.util.Map;
 @Controller
 @RequestMapping("/app/common/")
 public class AppUserController {
+    Logger logger = LoggerFactory.getLogger(AppUserController.class);
     @Resource
     AppServiceImpl appService;
 
     @RequestMapping("/addUserId2DeepLink")
     public ModelAndView get(@RequestParam String deepLink, @RequestParam String website) {
         String deviceId = (String) Context.currentContext().get(StaticContext.DEVICE_ID);
-        DeviceInfoVo deviceInfo = (DeviceInfoVo) Context.currentContext().get(Context.DEVICE_INFO);
-        SearchIO sio = new SearchIO("", "", "", website, "", deviceInfo.getMarketChannel(), deviceId, 0, 0);
         ModelAndView modelAndView = new ModelAndView();
         Map map = new HashMap();
+        DeviceInfoVo deviceInfo = null;
+        try {
+            deviceInfo = (DeviceInfoVo) Context.currentContext().get(Context.DEVICE_INFO);
+        } catch (Exception e) {
+            logger.error("usertoken  parameter is not sent ");
+            map.put("deeplink", deepLink);
+            modelAndView.addObject("data", map);
+            return modelAndView;
+        }
+        SearchIO sio = new SearchIO("", "", "", website, "", deviceInfo.getMarketChannel(), deviceId, 0, 0);
         UrmUser urmUser = appService.getUserByUserToken((String) Context.currentContext().get(StaticContext.USER_TOKEN));
         String affs[] = null;
         if (urmUser != null) {
