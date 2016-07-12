8dc0ddefb2e36eb842d81add6f5cc2617e67e696
zhangchengwei <zhangchengwei@hasoffer.com>
2016-07-12 17:11:49 +0800

    jar 包执行

--- hasoffer.runjar/src/main/java/hasoffer/runjar/run/RunTaskMain.java
+++ hasoffer.runjar/src/main/java/hasoffer/runjar/run/RunTaskMain.java
@@ -8,7 +8,7 @@ import hasoffer.runjar.util.SpringUtils;
  * Created by chevy on 2016/7/12.
  */
 
-public class TestRun {
+public class RunTaskMain {
 
     public static void main(String[] args) {
         IProductService productService = SpringUtils.getBean(IProductService.class);//ctx.getBean(IProductService.class);
