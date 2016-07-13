d1a9f8c316a71a88f58b56aec95a44cb9bd3801b
zhangchengwei <zhangchengwei@hasoffer.com>
2016-07-13 20:06:36 +0800

    shopclues url

--- hasoffer.fetch/src/main/java/hasoffer/fetch/sites/shopclues/ShopcluesHelper.java
+++ hasoffer.fetch/src/main/java/hasoffer/fetch/sites/shopclues/ShopcluesHelper.java
@@ -71,7 +71,7 @@ public class ShopcluesHelper {
         String url = "http://www.shopclues.com/apple-iphone-5s-16gb-44.html";
         String target = "http://tracking.vcommission.com/aff_c?offer_id=122&aff_id=48424&url=http%3A%2F%2Faffiliateshopclues.com%2F%3Fa%3D9%26c%3D19%26p%3Dr%26s1%3D%7Baffiliate_id%7D%26s2%3D%7Btransaction_id%7D%26ckmrdr%3Dhttp%3A%2F%2Fwww.shopclues.com%2Fmtech-v4-black-16gb-java-enabled-preloaded-whats-app-mobile-phone.html%3Futm_source%3Dvcommission%26utm_medium%3DCPS%26s2%3Dhomepage";
 
-        String deeplinkWithAff = getUrlWithAff(getUrlWithAff(url, null), null);
+        String deeplinkWithAff = getDeeplinkWithAff(getUrlWithAff(url, null), null);
         System.out.println(deeplinkWithAff);
         System.out.println(getKeywordFromSkuUrl(url));
     }
@@ -92,6 +92,7 @@ public class ShopcluesHelper {
     }
 
     public static String getDeeplinkWithAff(String url, String[] affs) {
-        return getCleanUrl(url) + "?ty=0&id=111438445&mcid=aff&utm_source=Hasoffer&OfferId=15";
+        return getUrlWithAff(getCleanUrl(url), affs);
+//        return getCleanUrl(url) + "?ty=0&id=111438445&mcid=aff&utm_source=Hasoffer&OfferId=15";
     }
 }
