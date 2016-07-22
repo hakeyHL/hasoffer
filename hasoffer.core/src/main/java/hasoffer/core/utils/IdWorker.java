f9181a5a20d5f12fd535099f94e616c1a1164af4
wing-SF <18735117232@163.com>
2016-07-22 16:59:39 +0800

    idword test

--- hasoffer.core/src/main/java/hasoffer/core/utils/IdWorker.java
+++ hasoffer.core/src/main/java/hasoffer/core/utils/IdWorker.java
@@ -1,162 +1,169 @@
 package hasoffer.core.utils;
 
-import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.security.SecureRandom;
-import java.util.regex.Pattern;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
 
+/**
+ * Created by glx on 2016/7/14.
+ */
 public class IdWorker {
-    private static final long twepoch = 1463464402094L;//永远不要更新这个这个值   NEVER UPDATE THIS VALUE
-    private static final String IDWORKER_CONFIG = "IDWORKER_CONFIG";
-    private static Logger logger = LoggerFactory.getLogger(IdWorker.class);
-    private static String IDWORKER_CONFIG_VALUE;
+    private static final Logger logger = LoggerFactory.getLogger(IdWorker.class);
+    private static final int ID_LEN = 16;
+    private static final long TWEPOCH = (new Date(2016 - 1900, 7 - 1, 22, 13, 28, 0)).getTime();//系统运行后，永远不要更新这个值   NEVER UPDATE THIS VALUE
+    private static final Map<String, IdWorker> MAP = new HashMap<String, IdWorker>();
 
-    static {
-        //获取环境变量
-        IDWORKER_CONFIG_VALUE = System.getenv(IDWORKER_CONFIG);
-        //判断环境变量是否合法
-        if (!configVerify(IDWORKER_CONFIG_VALUE)) {
-            logger.warn("无效的IDWORKER_CONFIG，使用默认值1_1");
-            IDWORKER_CONFIG_VALUE = "1_1";
+    private static final long MAX_ID = 0x7FFFFFFFFFFFFFFFL;
+
+    private static final long MAX_SECOND = 0x7FFFFFFFL;
+    private static final long MAX_MACHINE = 0xFFL;
+    private static final long MAX_SN = 0xFFFFFFL;
+    private final AtomicLong serialNumber = new AtomicLong(MAX_SN);//3个字�?;//四个字节
+    private long seconds = (System.currentTimeMillis() - TWEPOCH) / (1000L);
+    private int machine = 0;//1个字�?
+
+    private IdWorker(byte machine) {
+        if (machine > MAX_MACHINE) {
+            throw new RuntimeException();
         }
+
+        this.machine = machine;
     }
 
-    private long workerId;
-    private long datacenterId;
-    //毫秒级内序列号
-    private long sequence = 0L;
-    //起始纪元时间(2016-05-17 13:53:22)，时间标识 = 当前时间-此时间
-    //工作机器占用bit数
-    private long workerIdBits = 6L;
-    //处理中心占用bit数
-    private long datacenterIdBits = 3L;
-    //最大工作机器ID 111111 -> 64
-    private long maxWorkerId = -1L ^ (-1L << workerIdBits);
-    //最大处理中心ID 111 -> 8
-    private long maxDatacenterId = -1L ^ (-1L << datacenterIdBits);
-    //毫秒级内序列号占用bit数
-    private long sequenceBits = 12L;
-    //工作机器ID左偏移量 12
-    private long workerIdShift = sequenceBits;
-    //处理中心ID左偏移量 12+6=18
-    private long datacenterIdShift = sequenceBits + workerIdBits;
-    //时间标识左偏移量 12+6+3=21
-    private long timestampLeftShift = sequenceBits + workerIdBits + datacenterIdBits;
-    //毫秒级内序列号掩码（最大值）
-    private long sequenceMask = -1L ^ (-1L << sequenceBits);
-    //上次生成ID的时间标记
-    private long lastTimestamp = -1L;
+    public synchronized static IdWorker getInstance(Class<?> cls, byte machine) {
+        if (MAP.get(cls.getName()) == null) {
+            MAP.put(cls.getName(), new IdWorker(machine));
+        }
 
-    /**
-     * 创建ID生成器
-     */
-    public IdWorker() {
-        String[] configArgs = IDWORKER_CONFIG_VALUE.split("_");
-        long sysDatacenterId = String2long(configArgs[0]);
-        long sysWorkerId = String2long(configArgs[1]);
-        // 验证输入的工作机器ID是否合法
-        if (sysWorkerId > maxWorkerId || sysWorkerId < 0) {
-            throw new IllegalArgumentException(
-                    String.format("工作机器ID不能大于%d或者小于0!", maxWorkerId));
-        }
-        //验证输入的处理中心ID是否合法
-        if (sysDatacenterId > maxDatacenterId || sysDatacenterId < 0) {
-            throw new IllegalArgumentException(
-                    String.format("处理中心ID不能大于%d或者小于0!", maxDatacenterId));
-        }
-        this.workerId = sysWorkerId;
-        this.datacenterId = sysDatacenterId;
+        return MAP.get(cls.getName());
     }
 
-    /**
-     * 验证环境变量的值是否符合要求
-     *
-     * @param environmentVar 环境变量的值
-     * @return 环境变量是否合法，合法返回true,非法返回false
-     */
-    protected static boolean configVerify(String environmentVar) {
-        logger.info("IDWORKER_CONFIG=" + environmentVar);
-        if (StringUtils.isEmpty(environmentVar)) {
-            return false;
+    public synchronized static IdWorker getInstance(Class<?> cls) {
+        if (MAP.get(cls.getName()) == null) {
+            String machineStr = System.getenv("ID_WORKER");
+
+            byte machine = 0;
+            if (machineStr != null && machineStr.trim().length() > 0) {
+                machine = (byte) Integer.parseInt(machineStr);
+            } else {
+                logger.warn("没有发现ID_WORKER，如果部署多个实例，必须对每台机器设置不同的ID_WORKER；例如ID_WORKER=1");
+            }
+            MAP.put(cls.getName(), new IdWorker(machine));
         }
-        String reg = "^[0-7]_[0-9]{1,2}$";
-        Pattern pattern = Pattern.compile(reg);
-        return pattern.matcher(environmentVar).matches();
+
+        return MAP.get(cls.getName());
     }
 
-    /**
-     * 获取全局自增唯一ID
-     *
-     * @return 全局自增唯一ID
-     */
-    public synchronized long nextId() {
-        long timestamp = timeGen();
+    public static Date getDate(String id) {
+        long s = getSecond(id);
+        return new Date(s * 1000 + TWEPOCH);
+    }
 
-        if (timestamp < lastTimestamp) {
-            throw new RuntimeException(String
-                    .format("时间被调回，导致上次生成ID时间大于本次时间，不能生成ID，两次相差%d毫秒！",
-                            lastTimestamp - timestamp));
+    public static Date getDate(long id) {
+        return getDate(Long.toHexString(id));
+    }
+
+
+    public static long getSecond(String id) {
+        String str = len(id, ID_LEN);
+        return (long) Long.parseLong(str.substring(0, 8), 16);
+    }
+
+    public static long getSecond(long id) {
+        return getSecond(Long.toHexString(id));
+    }
+
+    public static byte getMachine(String id) {
+        String str = len(id, ID_LEN);
+        return (byte) Integer.parseInt(str.substring(8, 10), 16);
+    }
+
+    public static byte getMachine(long id) {
+        return getMachine(Long.toHexString(id));
+    }
+
+
+    public static long getSn(String id) {
+        String str = len(id, ID_LEN);
+        return (long) Long.parseLong(str.substring(10, 16), 16);
+    }
+
+    public static long getSn(long id) {
+        return getSn(Long.toHexString(id));
+    }
+
+    private static String len(String str, int len) {
+        StringBuffer stringBuffer = new StringBuffer();
+        for (int i = 0; i < len - str.length(); i++) {
+            stringBuffer.append("0");
         }
 
-        //同一毫秒级内操作
-        if (lastTimestamp == timestamp) {
-            //毫秒级内序列号进行+1操作,并验证
-            sequence = (sequence + 1) & sequenceMask;
-            //当前毫秒内，序列号已用完（1111 1111 1111 -> 4096）
-            if (sequence == 0) {
-                //等待下一毫秒
-                timestamp = tilNextMillis(lastTimestamp);
+        return stringBuffer.toString() + str;
+    }
+
+    public static void main(String[] args) {
+        final IdWorker stringIdWorker = IdWorker.getInstance(IdWorker.class);
+        long t = (System.currentTimeMillis());
+
+        for (int i = 0; i < 1000; i++) {
+            long id = stringIdWorker.nextLong();
+
+
+            System.out.println(getDate(id));
+            /*new Thread(new Runnable() {
+                @Override
+                public void run() {
+                   String str =  stringIdWorker.next();
+                    System.out.println(str);
+
+                }
+            }).start();*/
+        }
+        System.out.println(System.currentTimeMillis() - t);
+
+
+    }
+
+    public long nextLong() {
+        return Long.parseLong(nextString(), 16);
+    }
+
+    public synchronized String nextString() {
+        long nowSecond = (System.currentTimeMillis() - TWEPOCH) / (1000L);
+        if (nowSecond > MAX_SECOND) {
+            throw new RuntimeException();
+        }
+
+        long sn = 0;
+        if (seconds == nowSecond) {
+            sn = serialNumber.addAndGet(1);
+            if (sn > MAX_SN) {
+                try {
+                    Thread.sleep(200);
+                } catch (InterruptedException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+                return nextString();
             }
         } else {
-            //非同一毫秒级内，理论上计数器归零
-            //sequence = 0;
-            //但为保证尾数随机性大一些，毫秒级计数器归为0-9的随机数
-            sequence = new SecureRandom().nextInt(10);
+            serialNumber.set(0);
+            seconds = nowSecond;
+            sn = serialNumber.addAndGet(1);
         }
 
-        //当前生成ID时间设置为最后生成时间
-        lastTimestamp = timestamp;
-        //进行bit拼接
-        return ((timestamp - twepoch) << timestampLeftShift) | (datacenterId << datacenterIdShift) | (
-                workerId << workerIdShift) | sequence;
-    }
+        String daysHex = len(Long.toHexString(seconds), 8);
 
-    /**
-     * 等待下一毫秒
-     *
-     * @param lastTimestamp 上次生成ID的时间 毫秒级
-     * @return 下一毫秒时间
-     */
-    protected long tilNextMillis(long lastTimestamp) {
-        long timestamp = timeGen();
-        while (timestamp <= lastTimestamp) {
-            timestamp = timeGen();
-        }
-        return timestamp;
-    }
+        String machineHex = len(Integer.toHexString(machine), 2);
 
-    /**
-     * 获取系统当前时间
-     *
-     * @return 当前系统时间的毫秒级值
-     */
-    protected long timeGen() {
-        return System.currentTimeMillis();
-    }
+        String snHex = len(Long.toHexString(sn), 6);
 
-    /**
-     * 字符串转long类型
-     *
-     * @param value long类型字符串
-     * @return long类型结果
-     */
-    protected long String2long(String value) {
-        try {
-            return Long.parseLong(value);
-        } catch (Exception e) {
-            throw new RuntimeException("配置的信息转换为long类型失败！");
-        }
+        String ret = daysHex + machineHex + snHex;
+
+        return ret.toUpperCase();
     }
 }
