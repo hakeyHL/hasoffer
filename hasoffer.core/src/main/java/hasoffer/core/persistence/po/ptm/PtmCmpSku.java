e6993dddb0608fc1dfcdb711c7f9767850ab9c82
wing-SF <18735117232@163.com>
2016-07-27 17:28:30 +0800

    deal bug fix

--- hasoffer.core/src/main/java/hasoffer/core/persistence/po/ptm/PtmCmpSku.java
+++ hasoffer.core/src/main/java/hasoffer/core/persistence/po/ptm/PtmCmpSku.java
@@ -31,7 +31,10 @@ public class PtmCmpSku implements Identifiable<Long> {
 
     private String skuTitle;// 带商品的color，size属性的
     private String title;
+
     private float price;
+    @ColumnDefault(value = "0")
+    private float cashBack;
 
     private String rating;
 
@@ -381,6 +384,14 @@ public class PtmCmpSku implements Identifiable<Long> {
         this.supportPayMethod = supportPayMethod;
     }
 
+    public float getCashBack() {
+        return cashBack;
+    }
+
+    public void setCashBack(float cashBack) {
+        this.cashBack = cashBack;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -390,6 +401,7 @@ public class PtmCmpSku implements Identifiable<Long> {
 
         if (productId != sku.productId) return false;
         if (Float.compare(sku.price, price) != 0) return false;
+        if (Float.compare(sku.cashBack, cashBack) != 0) return false;
         if (checked != sku.checked) return false;
         if (failLoadImage != sku.failLoadImage) return false;
         if (commentsNumber != sku.commentsNumber) return false;
@@ -437,6 +449,7 @@ public class PtmCmpSku implements Identifiable<Long> {
         result = 31 * result + (skuTitle != null ? skuTitle.hashCode() : 0);
         result = 31 * result + (title != null ? title.hashCode() : 0);
         result = 31 * result + (price != +0.0f ? Float.floatToIntBits(price) : 0);
+        result = 31 * result + (cashBack != +0.0f ? Float.floatToIntBits(cashBack) : 0);
         result = 31 * result + (rating != null ? rating.hashCode() : 0);
         result = 31 * result + (imagePath != null ? imagePath.hashCode() : 0);
         result = 31 * result + (smallImagePath != null ? smallImagePath.hashCode() : 0);
