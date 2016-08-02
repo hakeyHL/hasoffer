a0cd8331c80144cfe9512b89970e275a0e6dbb0d
Joe <zhouwendong006@163.com>
2016-08-02 12:39:20 +0800

    添加任务开始时间

--- hasoffer.fetch.provider/src/main/java/hasoffer/dubbo/api/fetch/service/impl/FetchDubboServiceImpl.java
+++ hasoffer.fetch.provider/src/main/java/hasoffer/dubbo/api/fetch/service/impl/FetchDubboServiceImpl.java
@@ -14,6 +14,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.annotation.Resource;
+import java.util.Date;
 
 public class FetchDubboServiceImpl implements IFetchDubboService {
     private Logger logger = LoggerFactory.getLogger(FetchDubboServiceImpl.class);
@@ -40,6 +41,7 @@ public class FetchDubboServiceImpl implements IFetchDubboService {
         if (fetchUrlResult == null) {
             fetchUrlResult = new FetchUrlResult(webSite, url);
             fetchUrlResult.setTaskStatus(TaskStatus.START);
+            fetchUrlResult.setDate(new Date());
             addFetchUrlTask(fetchUrlResult);
         }
 
