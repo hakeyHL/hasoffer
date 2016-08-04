10d76bf9167d71fef944b4b4b9c5eb36e682045e
wing-SF <18735117232@163.com>
2016-08-04 18:18:42 +0800

    add try catch for dubboupdate

---
+++ hasoffer.admin/src/main/java/hasoffer/admin/fetch/service/impl/FetchUpdateServiceImpl.java
@@ -0,0 +1,23 @@
+package hasoffer.admin.fetch.service.impl;
+
+import hasoffer.core.product.ICmpSkuService;
+import hasoffer.dubbo.api.fetch.service.IFetchUpdateService;
+import hasoffer.spider.model.FetchUrlResult;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+/**
+ * Created on 2016/8/4.
+ */
+@Service
+public class FetchUpdateServiceImpl implements IFetchUpdateService {
+
+    @Resource
+    ICmpSkuService cmpSkuService;
+
+    @Override
+    public void updatePtmCmpSkuInfo(FetchUrlResult fetchUrlResult) {
+
+    }
+}
