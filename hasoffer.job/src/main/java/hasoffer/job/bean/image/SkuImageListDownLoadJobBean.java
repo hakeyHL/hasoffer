2c465bdc5c8c3c2e135763ebe6b7bebd7ed329c3
wing-SF <18735117232@163.com>
2016-08-10 12:05:13 +0800

    add try catch for dubbo update

--- hasoffer.job/src/main/java/hasoffer/job/bean/image/SkuImageListDownLoadJobBean.java
+++ hasoffer.job/src/main/java/hasoffer/job/bean/image/SkuImageListDownLoadJobBean.java
@@ -1,7 +1,9 @@
 package hasoffer.job.bean.image;
 
+import hasoffer.base.model.PageableResult;
 import hasoffer.core.persistence.dbm.osql.IDataBaseManager;
 import hasoffer.core.persistence.po.ptm.PtmCmpSkuImage;
+import hasoffer.core.product.IPtmCmpSkuImageService;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.slf4j.Logger;
@@ -10,11 +12,10 @@ import org.springframework.scheduling.quartz.QuartzJobBean;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Created on 2016/8/9.
@@ -25,41 +26,71 @@ public class SkuImageListDownLoadJobBean extends QuartzJobBean {
 
     @Resource
     IDataBaseManager dbm;
+    @Resource
+    IPtmCmpSkuImageService ptmCmpSkuImageService;
 
     @Override
     protected void executeInternal(JobExecutionContext jobExecutionContext) throws JobExecutionException {
 
-        final int page = 1, PAGE_SIZE = 1000;
-
-        List<PtmCmpSkuImage> skuImageList = dbm.query("SELECT t FROM PtmCmpSkuImage t WHERE t.checked = 0", page, PAGE_SIZE);
-
-        final LinkedBlockingQueue<PtmCmpSkuImage> cmpSkuQueue = new LinkedBlockingQueue<PtmCmpSkuImage>();
-//        cmpSkuQueue.addAll(skus);
+        final ConcurrentLinkedQueue<PtmCmpSkuImage> ptmCmpSkuImageQueue = new ConcurrentLinkedQueue<PtmCmpSkuImage>();
 
         ExecutorService es = Executors.newCachedThreadPool();
 
-        int processCount = 20;
-        final AtomicInteger processorCount = new AtomicInteger(0);
+        es.execute(new Runnable() {
 
-        for (int i = 0; i < processCount; i++) {
-            es.execute(new Runnable() {
-                @Override
-                public void run() {
-                    processorCount.addAndGet(1);
+            @Override
+            public void run() {
 
-                    while (true) {
-//                        PtmCmpSku t = cmpSkuQueue.poll();
-//
-//                        if (t == null) {
-//                            break;
-//                        }
+                int page = 1;
+                int pageSize = 1000;
 
-//                        cmpSkuService.downloadImage2(t);
+                PageableResult<PtmCmpSkuImage> pageableResult = dbm.queryPage("SELECT t FROM PtmCmpSkuImage t WHERE t.fetched = 0", page, pageSize);
+
+                long totalPage = pageableResult.getTotalPage();
+
+                while (page < 2) {
+
+                    if (page > 1) {
+                        pageableResult = dbm.queryPage("SELECT t FROM PtmCmpSkuImage t WHERE t.fetched = 0", page, pageSize);
                     }
 
+                    List<PtmCmpSkuImage> ptmCmpSkuImageList = pageableResult.getData();
+
+                    ptmCmpSkuImageQueue.addAll(ptmCmpSkuImageList);
+
+                    page++;
+
                 }
-            });
-        }
+            }
+        });
+
+//        for (int i = 0; i < 10; i++) {
+        es.execute(new Runnable() {
+
+            @Override
+            public void run() {
+                while (true) {
+
+                    PtmCmpSkuImage ptmCmpSkuImage = ptmCmpSkuImageQueue.poll();
+
+                    if (ptmCmpSkuImage == null) {
+                        try {
+                            TimeUnit.SECONDS.sleep(5);
+                        } catch (InterruptedException e) {
+
+                        }
+                        continue;
+                    }
+
+                    boolean fetchResultFlag = ptmCmpSkuImageService.downloadPtmCmpSkuImage(ptmCmpSkuImage.getId());
+
+                    if (fetchResultFlag) {
+                        ptmCmpSkuImageService.updateFetchStatus(ptmCmpSkuImage.getId(), fetchResultFlag);
+                    }
+                }
+            }
+        });
+//        }
 
         while (true) {
             try {
@@ -67,21 +98,6 @@ public class SkuImageListDownLoadJobBean extends QuartzJobBean {
             } catch (Exception e) {
                 break;
             }
-
-            if (cmpSkuQueue.size() > 0) {
-                logger.info("queue size = " + cmpSkuQueue.size());
-                continue;
-            }
-
-            if (processorCount.get() > 0) {
-                logger.info("processorCount = " + processorCount.get());
-                continue;
-            }
-
-            break;
         }
-
-        logger.info("All jobs finished.");
-
     }
 }
