12ac37b213e2b9f9d2326db5a4703c0a63f87745
huli <hakeyhl@163.com>
2016-08-15 12:32:06 +0800

    1. 新版sdk的产品详情页sku参数反斜线去掉

--- hasoffer.api/src/main/java/hasoffer/api/controller/AppSkuController.java
+++ hasoffer.api/src/main/java/hasoffer/api/controller/AppSkuController.java
@@ -1,6 +1,8 @@
 package hasoffer.api.controller;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import hasoffer.api.helper.Httphelper;
 import hasoffer.base.utils.StringUtils;
 import hasoffer.core.persistence.dbm.nosql.IMongoDbManager;
 import hasoffer.core.persistence.mongo.PtmCmpSkuDescription;
@@ -14,9 +16,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -67,15 +69,16 @@ public class AppSkuController {
      * @return
      */
     @RequestMapping("info")
-    public ModelAndView getSkuInfo(@RequestParam(defaultValue = "0") Long id) {
-        ModelAndView modelAndView = new ModelAndView();
+    public String getSkuInfo(@RequestParam(defaultValue = "0") Long id, HttpServletResponse response) {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("errorCode", "00000");
+        jsonObject.put("msg", "ok");
+        //PropertyFilter propertyFilter = JsonHelper.filterProperty(new String[]{"ratingNum", "bestPrice", "priceOff", "backRate", "support", "price", "returnGuarantee", "freight"});
         PtmCmpSku ptmCmpSku = cmpSkuService.getCmpSkuById(id);
-        modelAndView.addObject("errorCode", "00000");
-        modelAndView.addObject("msg", "ok");
         if (ptmCmpSku != null) {
             logger.info(" has this sku " + id);
             PtmCmpSkuDescription ptmCmpSkuDescription = mongoDbManager.queryOne(PtmCmpSkuDescription.class, ptmCmpSku.getId());
-            logger.info("get sku description from  mongo " + ptmCmpSkuDescription == null ? " not null" : " is null");
+            logger.info("get sku totalWeigth from  mongo " + ptmCmpSkuDescription == null ? " not null" : " is null");
             Map map = new HashMap<>();
             if (ptmCmpSkuDescription != null) {
                 map.put("description", ptmCmpSkuDescription.getJsonDescription() == null ? "" : ptmCmpSkuDescription.getJsonDescription());//描述
@@ -84,8 +87,44 @@ public class AppSkuController {
             List<PtmCmpSkuImage> ptmCmpSkuImages = ptmCmpSkuImageService.findPtmCmpSkuImages(ptmCmpSku.getId());
             map.put("images", getImageArray(ptmCmpSkuImages));
             map.put("distribution", 5);
-            modelAndView.addObject("data", map);
+            jsonObject.put("data", JSONObject.toJSON(map));
         }
-        return modelAndView;
+        Httphelper.sendJsonMessage(JSON.toJSONString(jsonObject), response);
+        return null;
+    }
+
+    /**
+     * 根据sku的id获取sku详细信息
+     *
+     * @return
+     */
+    @RequestMapping("info")
+    public String getSkuInfo(@RequestParam(defaultValue = "0") Long id, HttpServletResponse response) {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("errorCode", "00000");
+        jsonObject.put("msg", "ok");
+        //PropertyFilter propertyFilter = JsonHelper.filterProperty(new String[]{"ratingNum", "bestPrice", "priceOff", "backRate", "support", "price", "returnGuarantee", "freight"});
+        PtmCmpSku ptmCmpSku = cmpSkuService.getCmpSkuById(id);
+        if (ptmCmpSku != null) {
+            logger.info(" has this sku " + id);
+            PtmCmpSkuDescription ptmCmpSkuDescription = mongoDbManager.queryOne(PtmCmpSkuDescription.class, ptmCmpSku.getId());
+            logger.info("get sku totalWeigth from  mongo " + ptmCmpSkuDescription == null ? " not null" : " is null");
+            Map map = new HashMap<>();
+            if (ptmCmpSkuDescription != null) {
+                map.put("description", ptmCmpSkuDescription.getJsonDescription() == null ? "" : ptmCmpSkuDescription.getJsonDescription());//描述
+                map.put("specs", JSONObject.toJSON(ptmCmpSkuDescription.getJsonParam()));//参数
+            }
+            List<PtmCmpSkuImage> ptmCmpSkuImages = ptmCmpSkuImageService.findPtmCmpSkuImages(ptmCmpSku.getId());
+            map.put("images", getImageArray(ptmCmpSkuImages));
+            map.put("distribution", 5);
+            jsonObject.put("data", JSONObject.toJSON(map));
+        }
+        Httphelper.sendJsonMessage(JSON.toJSONString(jsonObject), response);
+        return null;
+    }
+
+    public static void main(String[] args) {
+        String temp = "{\"Fabric Care:\":\"Hand wash at 30°C, Do not bleach, Mild Iron, Do not Tumble Dry, Line Dry in shade, wash separately, do not iron on decorations/print, Use mild detergents\",\"Sales Package\":\"1 Kurti\",\"Legging Available\":\"No\",\"Ideal For\":\"Women's\",\"Other details\":\"Stitched\",\"Neck\":\"Mandarin collar\"}";
+        System.out.println(temp);
     }
 }
