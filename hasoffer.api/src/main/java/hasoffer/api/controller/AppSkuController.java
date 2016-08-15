6789367d0e78692b2e33e40a792ee6499aeb7c8a
huli <hakeyhl@163.com>
2016-08-15 12:33:46 +0800

    1. 新版sdk的产品详情页sku参数反斜线去掉

--- hasoffer.api/src/main/java/hasoffer/api/controller/AppSkuController.java
+++ hasoffer.api/src/main/java/hasoffer/api/controller/AppSkuController.java
@@ -63,68 +63,38 @@ public class AppSkuController {
         return li;
     }
 
-    /**
-     * 根据sku的id获取sku详细信息
-     *
-     * @return
-     */
-    @RequestMapping("info")
-    public String getSkuInfo(@RequestParam(defaultValue = "0") Long id, HttpServletResponse response) {
-        JSONObject jsonObject = new JSONObject();
-        jsonObject.put("errorCode", "00000");
-        jsonObject.put("msg", "ok");
-        //PropertyFilter propertyFilter = JsonHelper.filterProperty(new String[]{"ratingNum", "bestPrice", "priceOff", "backRate", "support", "price", "returnGuarantee", "freight"});
-        PtmCmpSku ptmCmpSku = cmpSkuService.getCmpSkuById(id);
-        if (ptmCmpSku != null) {
-            logger.info(" has this sku " + id);
-            PtmCmpSkuDescription ptmCmpSkuDescription = mongoDbManager.queryOne(PtmCmpSkuDescription.class, ptmCmpSku.getId());
-            logger.info("get sku totalWeigth from  mongo " + ptmCmpSkuDescription == null ? " not null" : " is null");
-            Map map = new HashMap<>();
-            if (ptmCmpSkuDescription != null) {
-                map.put("description", ptmCmpSkuDescription.getJsonDescription() == null ? "" : ptmCmpSkuDescription.getJsonDescription());//描述
-                map.put("specs", JSONObject.toJSON(ptmCmpSkuDescription.getJsonParam()));//参数
-            }
-            List<PtmCmpSkuImage> ptmCmpSkuImages = ptmCmpSkuImageService.findPtmCmpSkuImages(ptmCmpSku.getId());
-            map.put("images", getImageArray(ptmCmpSkuImages));
-            map.put("distribution", 5);
-            jsonObject.put("data", JSONObject.toJSON(map));
-        }
-        Httphelper.sendJsonMessage(JSON.toJSONString(jsonObject), response);
-        return null;
-    }
-
-    /**
-     * 根据sku的id获取sku详细信息
-     *
-     * @return
-     */
-    @RequestMapping("info")
-    public String getSkuInfo(@RequestParam(defaultValue = "0") Long id, HttpServletResponse response) {
-        JSONObject jsonObject = new JSONObject();
-        jsonObject.put("errorCode", "00000");
-        jsonObject.put("msg", "ok");
-        //PropertyFilter propertyFilter = JsonHelper.filterProperty(new String[]{"ratingNum", "bestPrice", "priceOff", "backRate", "support", "price", "returnGuarantee", "freight"});
-        PtmCmpSku ptmCmpSku = cmpSkuService.getCmpSkuById(id);
-        if (ptmCmpSku != null) {
-            logger.info(" has this sku " + id);
-            PtmCmpSkuDescription ptmCmpSkuDescription = mongoDbManager.queryOne(PtmCmpSkuDescription.class, ptmCmpSku.getId());
-            logger.info("get sku totalWeigth from  mongo " + ptmCmpSkuDescription == null ? " not null" : " is null");
-            Map map = new HashMap<>();
-            if (ptmCmpSkuDescription != null) {
-                map.put("description", ptmCmpSkuDescription.getJsonDescription() == null ? "" : ptmCmpSkuDescription.getJsonDescription());//描述
-                map.put("specs", JSONObject.toJSON(ptmCmpSkuDescription.getJsonParam()));//参数
-            }
-            List<PtmCmpSkuImage> ptmCmpSkuImages = ptmCmpSkuImageService.findPtmCmpSkuImages(ptmCmpSku.getId());
-            map.put("images", getImageArray(ptmCmpSkuImages));
-            map.put("distribution", 5);
-            jsonObject.put("data", JSONObject.toJSON(map));
-        }
-        Httphelper.sendJsonMessage(JSON.toJSONString(jsonObject), response);
-        return null;
-    }
-
     public static void main(String[] args) {
         String temp = "{\"Fabric Care:\":\"Hand wash at 30°C, Do not bleach, Mild Iron, Do not Tumble Dry, Line Dry in shade, wash separately, do not iron on decorations/print, Use mild detergents\",\"Sales Package\":\"1 Kurti\",\"Legging Available\":\"No\",\"Ideal For\":\"Women's\",\"Other details\":\"Stitched\",\"Neck\":\"Mandarin collar\"}";
         System.out.println(temp);
+    }
+
+    /**
+     * 根据sku的id获取sku详细信息
+     *
+     * @return
+     */
+    @RequestMapping("info")
+    public String getSkuInfo(@RequestParam(defaultValue = "0") Long id, HttpServletResponse response) {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("errorCode", "00000");
+        jsonObject.put("msg", "ok");
+        //PropertyFilter propertyFilter = JsonHelper.filterProperty(new String[]{"ratingNum", "bestPrice", "priceOff", "backRate", "support", "price", "returnGuarantee", "freight"});
+        PtmCmpSku ptmCmpSku = cmpSkuService.getCmpSkuById(id);
+        if (ptmCmpSku != null) {
+            logger.info(" has this sku " + id);
+            PtmCmpSkuDescription ptmCmpSkuDescription = mongoDbManager.queryOne(PtmCmpSkuDescription.class, ptmCmpSku.getId());
+            logger.info("get sku totalWeigth from  mongo " + ptmCmpSkuDescription == null ? " not null" : " is null");
+            Map map = new HashMap<>();
+            if (ptmCmpSkuDescription != null) {
+                map.put("description", ptmCmpSkuDescription.getJsonDescription() == null ? "" : ptmCmpSkuDescription.getJsonDescription());//描述
+                map.put("specs", JSONObject.toJSON(ptmCmpSkuDescription.getJsonParam()));//参数
+            }
+            List<PtmCmpSkuImage> ptmCmpSkuImages = ptmCmpSkuImageService.findPtmCmpSkuImages(ptmCmpSku.getId());
+            map.put("images", getImageArray(ptmCmpSkuImages));
+            map.put("distribution", 5);
+            jsonObject.put("data", JSONObject.toJSON(map));
+        }
+        Httphelper.sendJsonMessage(JSON.toJSONString(jsonObject), response);
+        return null;
     }
 }
