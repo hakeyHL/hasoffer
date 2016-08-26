9a868455c7c6921d1cda80803ab9b2a65f1d2ab9
wing <18735117232@163.com>
2016-08-26 22:32:55 +0800

    fetch category mobile sku brand and model

--- hasoffer.task/src/main/java/hasoffer/task/worker/FetchMobileCategoryBrandModel.java
+++ hasoffer.task/src/main/java/hasoffer/task/worker/FetchMobileCategoryBrandModel.java
@@ -32,72 +32,80 @@ public class FetchMobileCategoryBrandModel implements Runnable {
     public void run() {
         while (true) {
 
-            System.out.println("thread alive");
-            PtmCmpSku ptmcmpsku = cmpSkuQueue.poll();
+            try {
 
-            if (ptmcmpsku == null) {
-                System.out.println("pull get null wait 5 seconds");
-                try {
-                    TimeUnit.SECONDS.sleep(5);
-                } catch (InterruptedException e) {
 
+                System.out.println("thread alive");
+                PtmCmpSku ptmcmpsku = cmpSkuQueue.poll();
+
+                if (ptmcmpsku == null) {
+                    System.out.println("pull get null wait 5 seconds");
+                    try {
+                        TimeUnit.SECONDS.sleep(5);
+                    } catch (InterruptedException e) {
+
+                    }
+                    continue;
                 }
-                continue;
-            }
 
-            long skuid = ptmcmpsku.getId();
-            Website website = ptmcmpsku.getWebsite();
-            String url = ptmcmpsku.getUrl();
+                long skuid = ptmcmpsku.getId();
+                Website website = ptmcmpsku.getWebsite();
+                String url = ptmcmpsku.getUrl();
 
-            TaskStatus taskStatus = fetchDubboService.getUrlTaskStatus(website, url);
+                System.out.println(skuid + "_" + website + "_" + url);
+                TaskStatus taskStatus = fetchDubboService.getUrlTaskStatus(website, url);
+                System.out.println(taskStatus);
 
-            FetchUrlResult fetchUrlResult = null;
+                FetchUrlResult fetchUrlResult = null;
 
-            //如果返回结果状态为running，那么将sku返回队列
-            if (TaskStatus.RUNNING.equals(taskStatus) || TaskStatus.START.equals(taskStatus)) {
-                cmpSkuQueue.add(ptmcmpsku);
-                System.out.println("taskstatus RUNNING for [" + skuid + "]");
-                return;
-            } else if (TaskStatus.STOPPED.equals(taskStatus)) {
-                System.out.println("taskstatus STOPPED for [" + skuid + "]");
-                return;
-            } else if (TaskStatus.EXCEPTION.equals(taskStatus)) {
-                System.out.println("taskstatus EXCEPTION for [" + skuid + "]");
-                return;
-            } else if (TaskStatus.NONE.equals(taskStatus)) {
-                cmpSkuQueue.add(ptmcmpsku);
-                if (Website.SNAPDEAL.equals(website) || Website.FLIPKART.equals(website) || Website.AMAZON.equals(website) || Website.EBAY.equals(website)) {
+                //如果返回结果状态为running，那么将sku返回队列
+                if (TaskStatus.RUNNING.equals(taskStatus) || TaskStatus.START.equals(taskStatus)) {
                     cmpSkuQueue.add(ptmcmpsku);
-                    fetchDubboService.sendUrlTask(ptmcmpsku.getWebsite(), ptmcmpsku.getUrl(), TaskLevel.LEVEL_2);
+                    System.out.println("taskstatus RUNNING for [" + skuid + "]");
+                    return;
+                } else if (TaskStatus.STOPPED.equals(taskStatus)) {
+                    System.out.println("taskstatus STOPPED for [" + skuid + "]");
+                    return;
+                } else if (TaskStatus.EXCEPTION.equals(taskStatus)) {
+                    System.out.println("taskstatus EXCEPTION for [" + skuid + "]");
+                    return;
+                } else if (TaskStatus.NONE.equals(taskStatus)) {
+                    cmpSkuQueue.add(ptmcmpsku);
+                    if (Website.SNAPDEAL.equals(website) || Website.FLIPKART.equals(website) || Website.AMAZON.equals(website) || Website.EBAY.equals(website)) {
+                        cmpSkuQueue.add(ptmcmpsku);
+                        fetchDubboService.sendUrlTask(ptmcmpsku.getWebsite(), ptmcmpsku.getUrl(), TaskLevel.LEVEL_2);
+                    }
+                    System.out.println("taskstatus NONE for [" + skuid + "] , resend success");
+                    return;
+                } else {//(TaskStatus.FINISH.equals(taskStatus)))
+                    System.out.println("taskstatus FINISH for [" + skuid + "]");
+                    fetchUrlResult = fetchDubboService.getProductsByUrl(skuid, ptmcmpsku.getWebsite(), ptmcmpsku.getUrl());
+
+                    FetchedProduct fetchedProduct = fetchUrlResult.getFetchProduct();
+
+                    System.out.println(JSONUtil.toJSON(fetchedProduct).toString() + "id=" + skuid);
+
+                    try {
+                        cmpSkuService.createDescription(ptmcmpsku, fetchedProduct);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+                    try {
+                        cmpSkuService.updateCmpSkuBySpiderFetchedProduct(skuid, fetchedProduct);
+                    } catch (Exception e) {
+
+                    }
+
+                    try {
+                        cmpSkuService.createPtmCmpSkuImage(skuid, fetchedProduct);
+                    } catch (Exception e) {
+
+                    }
+
                 }
-                System.out.println("taskstatus NONE for [" + skuid + "] , resend success");
-                return;
-            } else {//(TaskStatus.FINISH.equals(taskStatus)))
-                System.out.println("taskstatus FINISH for [" + skuid + "]");
-                fetchUrlResult = fetchDubboService.getProductsByUrl(skuid, ptmcmpsku.getWebsite(), ptmcmpsku.getUrl());
-
-                FetchedProduct fetchedProduct = fetchUrlResult.getFetchProduct();
-
-                System.out.println(JSONUtil.toJSON(fetchedProduct).toString() + "id=" + skuid);
-
-                try {
-                    cmpSkuService.createDescription(ptmcmpsku, fetchedProduct);
-                } catch (Exception e) {
-
-                }
-
-                try {
-                    cmpSkuService.updateCmpSkuBySpiderFetchedProduct(skuid, fetchedProduct);
-                } catch (Exception e) {
-
-                }
-
-                try {
-                    cmpSkuService.createPtmCmpSkuImage(skuid, fetchedProduct);
-                } catch (Exception e) {
-
-                }
-
+            }catch (Exception e){
+                e.printStackTrace();
             }
         }
     }
