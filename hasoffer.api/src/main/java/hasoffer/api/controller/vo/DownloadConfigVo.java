119ad0c1a70373d247edc6bca1048ab2b901237e
huli <hakeyhl@163.com>
2016-08-30 17:47:27 +0800

    1. 调试deal无link

--- hasoffer.api/src/main/java/hasoffer/api/controller/vo/DownloadConfigVo.java
+++ hasoffer.api/src/main/java/hasoffer/api/controller/vo/DownloadConfigVo.java
@@ -1,10 +1,6 @@
 package hasoffer.api.controller.vo;
 
-import com.alibaba.fastjson.JSON;
-import hasoffer.base.model.Website;
-
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -37,65 +33,6 @@ public class DownloadConfigVo {
     }
 
     public static void main(String[] args) {
-        Map<String, List<ThirdAppVo>> map = new HashMap<String, List<ThirdAppVo>>();
-        Map<String, List<ThirdAppVo>> map2 = new HashMap<String, List<ThirdAppVo>>();
-        List<Map<String, List<ThirdAppVo>>> apps = new ArrayList<Map<String, List<ThirdAppVo>>>();
-        Map m = new HashMap();
-        List<ThirdAppVo> li = new ArrayList<ThirdAppVo>();
-        ThirdAppVo appVo = new ThirdAppVo();
-        appVo.setLogoUrl("logoUrl");
-        appVo.setComments("80,000");
-        appVo.setDownloadLink("a");
-        appVo.setDownloads("3000,000");
-        appVo.setIntroduction(" yi ban ");
-        appVo.setRatings(4.5f);
-        appVo.setWebsite(Website.FLIPKART);
-        li.add(appVo);
-
-
-        ThirdAppVo appVo1 = new ThirdAppVo();
-        appVo1.setLogoUrl("logoUrl1");
-        appVo1.setComments("60,000");
-        appVo1.setDownloadLink("b");
-        appVo1.setDownloads("4000,000");
-        appVo1.setIntroduction(" yi ban  a");
-        appVo1.setRatings(4.9f);
-        appVo1.setWebsite(Website.SNAPDEAL);
-        li.add(appVo1);
-
-
-        map.put("GOOGLEPLAY", li);
-
-
-        List<ThirdAppVo> li2 = new ArrayList<ThirdAppVo>();
-        ThirdAppVo appVo2 = new ThirdAppVo();
-        appVo2.setLogoUrl("logoUrl");
-        appVo2.setComments("80,000");
-        appVo2.setDownloadLink("a");
-        appVo2.setDownloads("3000,000");
-        appVo2.setIntroduction(" yi ban ");
-        appVo2.setRatings(4.5f);
-        appVo2.setWebsite(Website.FLIPKART);
-        li2.add(appVo2);
-
-
-        ThirdAppVo appVo3 = new ThirdAppVo();
-        appVo3.setLogoUrl("logoUrl1");
-        appVo3.setComments("60,000");
-        appVo3.setDownloadLink("b");
-        appVo3.setDownloads("4000,000");
-        appVo3.setIntroduction(" yi ban  a");
-        appVo3.setRatings(4.9f);
-        appVo3.setWebsite(Website.SNAPDEAL);
-        li2.add(appVo3);
-
-        apps.add(map);
-        apps.add(map2);
-        map.put("GOOGLEPLAY", li);
-        map2.put("9APP", li2);
-        m.put("apps", apps);
-        String string = JSON.toJSONString(apps);
-        System.out.println(string);
     }
 
     public boolean isBoot() {
