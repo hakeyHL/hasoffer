f6b090aac8d9b105e88dda4eb72dbc257a3c1d31
wing-SF <18735117232@163.com>
2016-09-01 09:51:24 +0800

    price off notice push and exception repush

---
+++ hasoffer.task/src/main/java/hasoffer/task/controller/PriceOffNoticeTest.java
@@ -0,0 +1,29 @@
+package hasoffer.task.controller;
+
+import hasoffer.core.user.IPriceOffNoticeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
+
+/**
+ * Created on 2016/8/31.
+ */
+@Controller
+@RequestMapping(value = "/PriceOffNoticeTest")
+public class PriceOffNoticeTest {
+
+    @Resource
+    IPriceOffNoticeService priceOffNoticeService;
+
+    @RequestMapping("/start")
+    @ResponseBody
+    public String start() {
+
+        priceOffNoticeService.priceOffCheck(979);
+
+        return "ok";
+    }
+
+}
