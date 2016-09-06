30ccdda99a7d7103e1b90f51328a3be78adb74f1
allen <allen_email@163.com>
2016-09-06 20:15:48 +0800

    更新测试

--- hasoffer.job/src/main/java/hasoffer/job/service/impl/FetchTestServiceImpl.java
+++ hasoffer.job/src/main/java/hasoffer/job/service/impl/FetchTestServiceImpl.java
@@ -1,79 +1,80 @@
 package hasoffer.job.service.impl;
 
-import hasoffer.base.enums.TaskLevel;
-import hasoffer.base.model.PageableResult;
-import hasoffer.base.model.Website;
-import hasoffer.core.persistence.dbm.osql.IDataBaseManager;
-import hasoffer.dubbo.api.fetch.service.IFetchDubboService;
-import hasoffer.job.dto.FetchTestTaskDTO;
-import hasoffer.job.service.IFetchTestService;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import javax.annotation.Resource;
+
 import org.apache.commons.io.FileUtils;
-import org.apache.commons.lang.time.DateFormatUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.Resource;
-import java.io.File;
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import hasoffer.base.enums.TaskLevel;
+import hasoffer.base.model.PageableResult;
+import hasoffer.base.model.Website;
+import hasoffer.core.persistence.dbm.osql.IDataBaseManager;
+import hasoffer.core.persistence.po.ptm.PtmCmpSku;
+import hasoffer.dubbo.api.fetch.service.IFetchDubboService;
+import hasoffer.job.service.IFetchTestService;
 
 @Service("fetchTestService")
 public class FetchTestServiceImpl implements IFetchTestService {
 
     private Logger logger = LoggerFactory.getLogger(FetchTestServiceImpl.class);
 
+    private final SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+
     @Resource
     IDataBaseManager dbm;
 
-    @Resource(name = "fetchTimerDubboService")
+    @Resource
     IFetchDubboService fetchDubboService;
 
     @Override
-    public void commitTask() {
-        String hql = "select new hasoffer.job.dto.FetchTestTaskDTO(p.id,p.website,p.url) from SrmProductSearchCount s , PtmCmpSku p where p.productId=s.productId and s.ymd=?0 and p.website=?1 and s.productId is not null order by s.count desc";
+    public void initTask() {
+        String hql = "select p.* from PtmCmpSku p left join SrmProductSearchCount s on p.productId = s.productId   where s.ymd=?0 and p.website=?1 order by s.count desc";
         Calendar calendar = Calendar.getInstance();
-        calendar.add(Calendar.DATE, -5);
-        String dateStr = DateFormatUtils.format(calendar, "yyyyMMdd");
+        calendar.add(Calendar.DATE, -1);
+        String dateStr = sdf.format(calendar.getTime());
         List<Website> websiteList = Arrays.asList(Website.AMAZON, Website.FLIPKART, Website.SNAPDEAL, Website.SHOPCLUES,
                 Website.PAYTM);
-        String timeStr = DateFormatUtils.format(new Date(), "HH");
-        String homeFolder = System.getProperty("user.home");
-        String baseOutFolder = homeFolder + File.separator + "logs" + File.separator + "testFetch" + File.separator + dateStr + File.separator + timeStr;
+        long batch = System.currentTimeMillis();
+        String baseOutFolder = "/home/hasoffer/logs/testFetch/" + dateStr + "/" + batch;
 
-        ExecutorService executorService = Executors.newFixedThreadPool(websiteList.size());
-        int seconds = 60 * 20;
+        ExecutorService service = Executors.newFixedThreadPool(websiteList.size());
+
         for (Website website : websiteList) {
-            PageableResult<FetchTestTaskDTO> page = dbm.queryPage(hql, 1, 1000, Arrays.asList(dateStr, website));
-            logger.info("Fetch Test start: website:{},count:{}", website.toString(), page.getNumFund());
-            List<FetchTestTaskDTO> list = page.getData();
+            PageableResult<PtmCmpSku> page = dbm.queryPage(hql, 1, 1000, Arrays.asList(dateStr, website));
+            logger.debug("Fetch Test start: website:{},count:{}", website.toString(), page.getNumFund());
+            List<PtmCmpSku> list = page.getData();
             File file = new File(baseOutFolder + "/" + website.toString() + "_task.txt");
-            for (FetchTestTaskDTO ptmCmpSku : list) {
-                fetchDubboService.sendUrlTask(ptmCmpSku.getWebsite(), ptmCmpSku.getUrl(), seconds, TaskLevel.LEVEL_1);
+            for (PtmCmpSku ptmCmpSku : list) {
+                fetchDubboService.sendUrlTask(ptmCmpSku.getWebsite(), ptmCmpSku.getUrl(), TaskLevel.LEVEL_3);
                 try {
                     FileUtils.write(file, ptmCmpSku.toString() + "\r\n", "utf-8", true);
-                    logger.info("save task success ! " + file.getAbsolutePath() + "," + ptmCmpSku.toString());
                 } catch (IOException e) {
                     logger.error("save task error ! " + file.getAbsolutePath() + "," + ptmCmpSku.toString(), e);
                 }
             }
             //启动结果轮询线程
             File resultFile = new File(baseOutFolder + "/" + website.toString() + "_result.txt");
-            executorService.execute(new FetchTestWorker(list, fetchDubboService, resultFile));
+            service.execute(new FetchTestWorker(list, fetchDubboService, resultFile));
         }
 
-        executorService.shutdown();
+        service.shutdown();
         while (true) {
-            if (executorService.isTerminated()) {
+            if (service.isTerminated()) {
                 break;
             }
             try {
-                Thread.sleep(10000);
+                Thread.sleep(2000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
--- hasoffer.job/src/main/java/hasoffer/job/service/impl/FetchTestWorker.java
+++ hasoffer.job/src/main/java/hasoffer/job/service/impl/FetchTestWorker.java
@@ -1,60 +1,56 @@
 package hasoffer.job.service.impl;
 
-import hasoffer.base.enums.TaskStatus;
-import hasoffer.dubbo.api.fetch.service.IFetchDubboService;
-import hasoffer.job.dto.FetchTestTaskDTO;
-import hasoffer.spider.model.FetchUrlResult;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayDeque;
-import java.util.List;
-import java.util.Queue;
+import hasoffer.base.enums.TaskStatus;
+import hasoffer.core.persistence.po.ptm.PtmCmpSku;
+import hasoffer.dubbo.api.fetch.service.IFetchDubboService;
+import hasoffer.spider.model.FetchUrlResult;
 
 public class FetchTestWorker implements Runnable {
 
-    private final Logger logger = LoggerFactory.getLogger(FetchTestWorker.class);
+    private Logger logger = LoggerFactory.getLogger(FetchTestWorker.class);
 
-    private Queue<FetchTestTaskDTO> queue;
-    private IFetchDubboService fetchDubboService;
-    private File file;
+    List<PtmCmpSku> list;
+    IFetchDubboService fetchDubboService;
+    File file;
 
-    public FetchTestWorker(List<FetchTestTaskDTO> list, IFetchDubboService fetchDubboService, File file) {
-        this.queue = new ArrayDeque<>(list);
+    public FetchTestWorker(List<PtmCmpSku> list, IFetchDubboService fetchDubboService, File file) {
+        this.list = list;
         this.fetchDubboService = fetchDubboService;
         this.file = file;
     }
 
     @Override
     public void run() {
-        long expireSeconds = 20 * 60;
-        String lineSeparator = System.getProperty("line.separator");
-        while (!queue.isEmpty()) {
-            FetchTestTaskDTO ptmCmpSku = queue.poll();
-            if (ptmCmpSku != null) {
-                TaskStatus taskStatus = fetchDubboService.getUrlTaskStatus(ptmCmpSku.getWebsite(), ptmCmpSku.getUrl(), expireSeconds);
+        while (list != null && !list.isEmpty()) {
+            List<PtmCmpSku> resultList = new ArrayList<PtmCmpSku>();
+            for (PtmCmpSku ptmCmpSku : list) {
+                TaskStatus taskStatus = fetchDubboService.getUrlTaskStatus(ptmCmpSku.getWebsite(), ptmCmpSku.getUrl());
                 if (TaskStatus.FINISH.equals(taskStatus)) {
-                    FetchUrlResult fetchUrlResult = fetchDubboService.getProductsByUrl(
-                            ptmCmpSku.getWebsite(), ptmCmpSku.getUrl(), expireSeconds);
+                    FetchUrlResult fetchUrlResult = fetchDubboService.getProductsByUrl(ptmCmpSku.getId(),
+                            ptmCmpSku.getWebsite(), ptmCmpSku.getUrl());
                     try {
-                    	// taskId getTaskStatus url	skuId	price	website
                         FileUtils.write(file,
-                        		fetchUrlResult.getTaskId()+"\t"+fetchUrlResult.getTaskStatus().toString()+"\t"+ptmCmpSku.getUrl()+"\t"+ptmCmpSku.getId()+"\t"+fetchUrlResult.getFetchProduct().getPrice()+"\t"+fetchUrlResult.getWebsite().toString() + lineSeparator, "utf-8",
+                                fetchUrlResult.toStringByTab().replace("\r", " ").replace("\n", " ") + "\r\n", "utf-8",
                                 true);
                     } catch (IOException e) {
                         logger.error(e.getMessage());
                     }
 
-                } else if (TaskStatus.EXCEPTION.equals(taskStatus) || TaskStatus.STOPPED.equals(taskStatus)) {
-                    FetchUrlResult fetchUrlResult = fetchDubboService.getProductsByUrl(
-                            ptmCmpSku.getWebsite(), ptmCmpSku.getUrl(), expireSeconds);
-                    logger.error(fetchUrlResult.toString());
-                } else {
-                    queue.add(ptmCmpSku);
+                    resultList.add(ptmCmpSku);
                 }
+            }
+            if (!resultList.isEmpty()) {
+                list.removeAll(resultList);
+                resultList.clear();
             }
         }
     }
