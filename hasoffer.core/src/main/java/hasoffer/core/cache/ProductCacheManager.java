7581919d7181247ad2106e4c92a9b1c2a3c57619
huli <hakeyhl@163.com>
2016-09-07 19:40:37 +0800

    1. fix priceoff notice

--- hasoffer.core/src/main/java/hasoffer/core/cache/ProductCacheManager.java
+++ hasoffer.core/src/main/java/hasoffer/core/cache/ProductCacheManager.java
@@ -100,7 +100,7 @@ public class ProductCacheManager {
                 pagedCmpskus = productService.listOnsaleCmpSkus(proId, page, size);
                 List<PtmCmpSku> data = pagedCmpskus.getData();
                 if (data != null && data.size() > 0) {
-                    pagedCmpskus.setData(getOnsaleSkuList(data, proId));
+//                    pagedCmpskus.setData(getOnsaleSkuList(data, proId));
                     cacheService.add(key, JSONUtil.toJSON(pagedCmpskus), TimeUtils.SECONDS_OF_1_HOUR * 2);
                 }
             } else {
@@ -118,7 +118,6 @@ public class ProductCacheManager {
                     if (StringUtils.isEmpty(website) || price == null) {
                         continue;
                     }
-
                     cmpSku.setId(Long.valueOf(map.get("id") + ""));
                     cmpSku.setProductId(Long.valueOf(map.get("productId") + ""));
                     cmpSku.setWebsite(Website.valueOf(website));
