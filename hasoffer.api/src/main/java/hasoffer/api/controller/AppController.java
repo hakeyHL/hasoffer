6765d55ee80f62d405331a3af4bc939246f0bbce
huli <hakeyhl@163.com>
2016-09-13 09:57:07 +0800

    1. remove key features from Deal list

--- hasoffer.api/src/main/java/hasoffer/api/controller/AppController.java
+++ hasoffer.api/src/main/java/hasoffer/api/controller/AppController.java
@@ -709,9 +709,9 @@ public class AppController {
             sb.append("1 Click \"Activate Deal\" button.\n");
             sb.append("2 Add the product of your choice to cart.\n");
             sb.append("3 And no coupon code required.\n\n");
-            sb.append("Key Features: \n");
             PtmCmpSkuDescription ptmCmpSkuDescription = mongoDbManager.queryOne(PtmCmpSkuDescription.class, appDeal.getPtmcmpskuid());
             if (ptmCmpSkuDescription != null) {
+                sb.append("Key Features: \n");
                 String jsonParam = ptmCmpSkuDescription.getJsonParam();
                 Map jsonMap = JsonHelper.getJsonMap(jsonParam);
                 //遍历map
