988f2331e8e39f93a7ceea054f2ae0d1defec912
wing-SF <18735117232@163.com>
2016-09-20 17:35:42 +0800

    admin push bug fix

--- hasoffer.job/src/main/java/hasoffer/job/bean/push/RePushPriceOffNoticeJobBean.java
+++ hasoffer.job/src/main/java/hasoffer/job/bean/push/RePushPriceOffNoticeJobBean.java
@@ -37,7 +37,7 @@ public class RePushPriceOffNoticeJobBean extends QuartzJobBean {
 
             Object pop = null;
 
-            if (TimeUtils.getHour() < 18) {
+            if (TimeUtils.getHour() < 14) {
                 pop = redisListService.pop(PUSH_FAIL_PRICEOFFNOTICE_ID);
             } else if (TimeUtils.getHour() > 20) {
                 pop = redisListService.pop(PUSH_FAIL_PRICEOFFNOTICE_ID_22);
