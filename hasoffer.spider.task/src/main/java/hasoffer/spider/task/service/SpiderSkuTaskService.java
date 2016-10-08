5a66849df7179c30b381ad50f3f7a89f96373918
Joe <zhouwendong006@163.com>
2016-10-08 23:02:46 +0800

    修改执行时间

--- hasoffer.spider.task/src/main/java/hasoffer/spider/task/service/SpiderSkuTaskService.java
+++ hasoffer.spider.task/src/main/java/hasoffer/spider/task/service/SpiderSkuTaskService.java
@@ -45,20 +45,20 @@ public class SpiderSkuTaskService {
         String pageSql = "SELECT sku.id,sku.url,sku.website,sku.productId as countNum FROM SrmProductSearchCount psc LEFT JOIN ptmcmpsku sku ON psc.productId = sku.productId WHERE psc.ymd = :ymd AND psc.count > :sum AND sku.website = :website AND sku.`status` <> 'OFFSALE' limit :begin,:end";
         int pageSize = 1000;
         int count = 0;
-        logger.info("Query Sql:SELECT count(*) as countNum FROM SrmProductSearchCount psc LEFT JOIN ptmcmpsku sku ON psc.productId = sku.productId WHERE psc.ymd = {} AND psc.count > {} AND sku.website = {} AND sku.`status` <> 'OFFSALE'", dateStr, num, webSite.name());
-        logger.info("Count Amazon:page count:{} and pageSize:1000", count);
 
         Map<String, Object> paramsMap = new HashMap<>();
         paramsMap.put("ymd", dateStr);
         paramsMap.put("sum", num);
         paramsMap.put("website", webSite.name());
         List list = dbm.queryBySql(countSql, paramsMap);
+        logger.info("Query Sql:SELECT count(*) as countNum FROM SrmProductSearchCount psc LEFT JOIN ptmcmpsku sku ON psc.productId = sku.productId WHERE psc.ymd = {} AND psc.count > {} AND sku.website = '{}' AND sku.`status` <> 'OFFSALE'", dateStr, num, webSite.name());
         for (Object obj : list) {
             if (obj != null) {
                 Map<String, Object> temp = (Map<String, Object>) obj;
                 count = (int) temp.get("countNum");
             }
         }
+        logger.info("Count Amazon:page count:{} and pageSize:1000", count);
 
         int countPage = count / pageSize;
         if (count % pageSize > 0) {
