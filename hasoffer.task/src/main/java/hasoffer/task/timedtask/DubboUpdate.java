0a8035a72707f2d9b1f14436d4d3f63bb67a77c8
wing-SF <18735117232@163.com>
2016-10-11 11:54:12 +0800

    queue wait sku number above 5w and time above 10min drop 1w

--- hasoffer.task/src/main/java/hasoffer/task/timedtask/DubboUpdate.java
+++ hasoffer.task/src/main/java/hasoffer/task/timedtask/DubboUpdate.java
@@ -91,7 +91,7 @@ public class DubboUpdate {
      * 缓存时间设置为30min
      * @return
      */
-    @Scheduled(cron = "00 55 11 * * ?")
+    @Scheduled(cron = "00 15 12 * * ?")
     public void priceOffNotieUpdatestart() {
 
         long startTime = TimeUtils.now();
