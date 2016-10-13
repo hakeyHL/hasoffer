54259c5701b15907ea8a49478d0c94b7c0305c5c
Joe <zhouwendong006@163.com>
2016-10-13 18:00:04 +0800

    同步整体代码

--- hasoffer.spider.provider/src/test/java/hasoffer/spider/test/SpiderConfigServiceTest.java
+++ hasoffer.spider.provider/src/test/java/hasoffer/spider/test/SpiderConfigServiceTest.java
@@ -1,18 +1,15 @@
 package hasoffer.spider.test;
 
-import hasoffer.spider.service.ISpiderConfigService;
+import hasoffer.dubbo.spider.task.api.ISpiderConfigService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import javax.annotation.Resource;
-
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "classpath:applicationContext-resources.xml")
 public class SpiderConfigServiceTest {
 
-    @Resource
     ISpiderConfigService spiderConfigService;
 
     @Test
