072ed1a4b03753373b930396f2c57abb99335bc7
huli <hakeyhl@163.com>
2016-10-17 13:26:58 +0800

    1. add home page sign tip copy content .

---
+++ hasoffer.api/src/main/java/hasoffer/api/controller/AppCliCfgController.java
@@ -0,0 +1,37 @@
+package hasoffer.api.controller;
+
+import hasoffer.api.controller.vo.ResultVo;
+import hasoffer.core.app.AppClientCfgService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Arrays;
+
+/**
+ * Created by hs on 2016年10月17日.
+ * Time 12:23
+ * 客户端配置参数controller
+ */
+@Controller
+@RequestMapping(value = "/cfg")
+public class AppCliCfgController {
+    private Logger logger = LoggerFactory.getLogger(AppCliCfgController.class);
+    @Autowired
+    private AppClientCfgService appClientCfgService;
+
+    public static void main(String[] args) {
+
+    }
+
+    @RequestMapping(value = "/app/homeCfg")
+    public ModelAndView homePageRewardsConfig() {
+        ResultVo resultVo = new ResultVo();
+        resultVo.getData().put("redeem", Arrays.asList("Earn Hasoffer Coin Everyday!", "Redeem Rs100-1000 Amazon Gift Card"));
+        return null;
+    }
+
+}
