95223be0a72fae4a5822877b1414c498b431ef44
zhangchengwei <zhangchengwei@hasoffer.com>
2016-10-21 11:56:20 +0800

    jdbc config

--- hasoffer.offlinejob/src/main/java/hasoffer/job/bean/image/SkuImageDownloadJobBean.java
+++ hasoffer.offlinejob/src/main/java/hasoffer/job/bean/image/SkuImageDownloadJobBean.java
@@ -1,6 +1,11 @@
 package hasoffer.job.bean.image;
 
+import hasoffer.base.model.PageableResult;
+import hasoffer.base.utils.ArrayUtils;
+import hasoffer.base.utils.StringUtils;
 import hasoffer.core.persistence.dbm.osql.IDataBaseManager;
+import hasoffer.core.persistence.dbm.osql.datasource.DataSource;
+import hasoffer.core.persistence.dbm.osql.datasource.DataSourceType;
 import hasoffer.core.persistence.po.ptm.PtmCmpSku;
 import hasoffer.core.product.ICmpSkuService;
 import org.quartz.JobExecutionContext;
@@ -23,7 +28,8 @@ public class SkuImageDownloadJobBean extends QuartzJobBean {
      * 取ptmimage 逻辑：未下载下来的图片，按照失败次数从小到大排
      */
     private static final String Q_SKU_IMAGE =
-            "SELECT t FROM PtmCmpSku t WHERE t.imagePath IS NULL AND t.oriImageUrl IS NOT NULL AND t.failLoadImage = 0";
+            "SELECT t FROM PtmCmpSku t WHERE t.imagePath IS NULL";
+    //            "SELECT t FROM PtmCmpSku t WHERE t.imagePath IS NULL AND t.oriImageUrl IS NOT NULL AND t.failLoadImage = 0";
     @Resource
     ICmpSkuService cmpSkuService;
     @Resource
@@ -32,14 +38,16 @@ public class SkuImageDownloadJobBean extends QuartzJobBean {
 
     @Override
     protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
-        final int page = 1, PAGE_SIZE = 1000;
-
-        List<PtmCmpSku> skus = dbm.query(Q_SKU_IMAGE, page, PAGE_SIZE);
 
         final LinkedBlockingQueue<PtmCmpSku> cmpSkuQueue = new LinkedBlockingQueue<PtmCmpSku>();
-        cmpSkuQueue.addAll(skus);
 
         ExecutorService es = Executors.newCachedThreadPool();
+        es.execute(new Runnable() {
+            @Override
+            public void run() {
+                loadImageDownLoadTasks(cmpSkuQueue);
+            }
+        });
 
         int processCount = 20;
         final AtomicInteger processorCount = new AtomicInteger(0);
@@ -86,5 +94,39 @@ public class SkuImageDownloadJobBean extends QuartzJobBean {
         }
 
         logger.info("All jobs finished.");
+    }
+
+    @DataSource(value = DataSourceType.Slave)
+    private void loadImageDownLoadTasks(LinkedBlockingQueue<PtmCmpSku> cmpSkusQueue) {
+        final int PAGE_SIZE = 1000;
+
+        PageableResult<PtmCmpSku> pSkus = dbm.queryPage(Q_SKU_IMAGE, 1, PAGE_SIZE);
+
+        long totalPage = pSkus.getTotalPage();
+        long page = totalPage;
+
+        while (page >= 1) {
+            List<PtmCmpSku> skus = null;
+            if (page == 1) {
+                skus = pSkus.getData();
+            } else {
+                skus = dbm.query(Q_SKU_IMAGE, (int) page, PAGE_SIZE);
+            }
+
+            if (ArrayUtils.hasObjs(skus)) {
+                for (PtmCmpSku sku : skus) {
+                    if (StringUtils.isEmpty(sku.getOriImageUrl())) {
+                        continue;
+                    }
+                    if (sku.isFailLoadImage()) {
+                        continue;
+                    }
+
+                    cmpSkusQueue.add(sku);
+                }
+            }
+
+            page--;
+        }
     }
 }
