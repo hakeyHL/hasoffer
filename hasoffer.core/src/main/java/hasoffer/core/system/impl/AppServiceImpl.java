e08fea5d680b7733b58bf2b814d338a41c529271
Joe <zhouwendong006@163.com>
2016-10-29 19:20:44 +0800

    亚马逊50次截一次

--- hasoffer.core/src/main/java/hasoffer/core/system/impl/AppServiceImpl.java
+++ hasoffer.core/src/main/java/hasoffer/core/system/impl/AppServiceImpl.java
@@ -460,12 +460,11 @@ public class AppServiceImpl implements IAppService {
 
     private String getAmazonIndexUrl(MarketChannel marketChannel, String deviceId) {
         Random random = new Random();
-//        if (random.nextInt(50) == 1) {
-//            return "intent://amazon.in/?tag=ahaso1-21&camp=3638&ref=as_li_tl#Intent;scheme=com.amazon.mobile.shopping;package=in.amazon.mShop.android.shopping;S.browser_fallback_url=https://play.google.com/store/apps/details?id=in.amazon.mShop.android.shopping;end";
-//        }
-        String amazonUrl = "intent://amazon.in/?tag=ahaso1-21&camp=3638&ref=as_li_tl#Intent;scheme=com.amazon.mobile.shopping;package=in.amazon.mShop.android.shopping;S.browser_fallback_url=https://play.google.com/store/apps/details?id=in.amazon.mShop.android.shopping;end";
+        String amazonUrl = "";
+        if (random.nextInt(50) == 1) {
+            amazonUrl = "intent://amazon.in/?tag=ahaso1-21&camp=3638&ref=as_li_tl#Intent;scheme=com.amazon.mobile.shopping;package=in.amazon.mShop.android.shopping;S.browser_fallback_url=https://play.google.com/store/apps/details?id=in.amazon.mShop.android.shopping;end";
+        }
         return new String(org.apache.commons.codec.binary.Base64.encodeBase64(amazonUrl.getBytes(Charset.forName("UTF-8"))));
-//        return "";
     }
 
 
@@ -533,13 +532,13 @@ public class AppServiceImpl implements IAppService {
     }
 
     private String getAliExpressIndexUrl(MarketChannel marketChannel, String deviceId) {
-        return "";
         //Random random = new Random();
         //if (random.nextInt(30) == 1) {
         //    String url = "http://s.click.aliexpress.com/e/";
         //    String[] aliExpresses = new String[]{"qbA6QFyv3", "dkAEi2de1"};
         //    url = url + aliExpresses[random.nextInt(aliExpresses.length)];
-        //    return new String(org.apache.commons.codec.binary.Base64.encodeBase64(url.getBytes(Charset.forName("UTF-8"))));
+        String url = "";
+        return new String(org.apache.commons.codec.binary.Base64.encodeBase64(url.getBytes(Charset.forName("UTF-8"))));
         //}else {
         //    return "";
         //}
