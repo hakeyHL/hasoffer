f80306c4bf24d55118fff3485e2d979d7d7f81b2
wing-SF <18735117232@163.com>
2016-11-02 21:04:48 +0800

    sku update update

--- hasoffer.task/src/main/java/hasoffer/task/worker/ListNeedUpdateFromRedisWorker.java
+++ hasoffer.task/src/main/java/hasoffer/task/worker/ListNeedUpdateFromRedisWorker.java
@@ -60,62 +60,68 @@ public class ListNeedUpdateFromRedisWorker implements Runnable {
             }
 
             //队列取数
-            Object pop = redisListService.pop(UPDATE_WAIT_QUEUE + ymd);
-            if (pop == null) {//如果队列没有数据了，休息30分钟
-                try {
-                    TimeUnit.MINUTES.sleep(30);
-                } catch (InterruptedException e) {
+            //num默认为更新线程数量
+            int num = 60;
+            while (num > 0) {
+                num--;
 
+                Object pop = redisListService.pop(UPDATE_WAIT_QUEUE + ymd);
+                if (pop == null) {//如果队列没有数据了，休息30分钟
+                    try {
+                        TimeUnit.MINUTES.sleep(30);
+                    } catch (InterruptedException e) {
+
+                    }
+                    continue;
                 }
-                continue;
-            }
 
-            //根据商品id，发起更新任务
-            Long productId = Long.valueOf((String) pop);
+                //根据商品id，发起更新任务
+                Long productId = Long.valueOf((String) pop);
 
-            List<PtmCmpSku> ptmCmpSkuList = cmpSkuService.listCmpSkus(productId);
+                List<PtmCmpSku> ptmCmpSkuList = cmpSkuService.listCmpSkus(productId);
 
-            //在加入队列的时候进行一些必要的判断
-            if (ptmCmpSkuList != null && ptmCmpSkuList.size() > 0) {
+                //在加入队列的时候进行一些必要的判断
+                if (ptmCmpSkuList != null && ptmCmpSkuList.size() > 0) {
 
-                for (PtmCmpSku sku : ptmCmpSkuList) {
+                    for (PtmCmpSku sku : ptmCmpSkuList) {
 
-                    //offsale的不再更新
-                    if (SkuStatus.OFFSALE.equals(sku.getStatus())) {
-                        continue;
-                    }
-
-                    Website website = sku.getWebsite();
-
-                    //暂时过滤掉myntra
-                    if (Website.MYNTRA.equals(website)) {
-                        continue;
-                    }
-
-                    //高优先级的网站
-                    if (Website.SNAPDEAL.equals(website) || Website.FLIPKART.equals(website) || Website.AMAZON.equals(website)) {
-
-                        //过滤掉snapdeal中viewAllSeller的情况
-                        if (Website.SNAPDEAL.equals(website)) {
-                            String url = sku.getUrl();
-                            url = StringUtils.filterAndTrim(url, Arrays.asList("/viewAllSellers"));
-                            sku.setUrl(url);
-                        }
-                        //过滤掉amazon中gp/offer-listing的url,该url没有描述等信息
-                        if (Website.AMAZON.equals(website)) {
-                            String url = sku.getUrl();
-                            url = url.replace("gp/offer-listing", "dp");
-                            sku.setUrl(url);
+                        //offsale的不再更新
+                        if (SkuStatus.OFFSALE.equals(sku.getStatus())) {
+                            continue;
                         }
 
-                        queue.add(sku);
-                        fetchDubboService.sendUrlTask(sku.getWebsite(), sku.getUrl(), cacheSeconds, TaskLevel.LEVEL_3);
-                    } else {
-                        queue.add(sku);
-                        fetchDubboService.sendUrlTask(sku.getWebsite(), sku.getUrl(), cacheSeconds, TaskLevel.LEVEL_5);
-                    }
+                        Website website = sku.getWebsite();
 
-                    logger.info("send url request succes for " + sku.getWebsite() + " sku id is [" + sku.getId() + "]");
+                        //暂时过滤掉myntra
+                        if (Website.MYNTRA.equals(website)) {
+                            continue;
+                        }
+
+                        //高优先级的网站
+                        if (Website.SNAPDEAL.equals(website) || Website.FLIPKART.equals(website) || Website.AMAZON.equals(website)) {
+
+                            //过滤掉snapdeal中viewAllSeller的情况
+                            if (Website.SNAPDEAL.equals(website)) {
+                                String url = sku.getUrl();
+                                url = StringUtils.filterAndTrim(url, Arrays.asList("/viewAllSellers"));
+                                sku.setUrl(url);
+                            }
+                            //过滤掉amazon中gp/offer-listing的url,该url没有描述等信息
+                            if (Website.AMAZON.equals(website)) {
+                                String url = sku.getUrl();
+                                url = url.replace("gp/offer-listing", "dp");
+                                sku.setUrl(url);
+                            }
+
+                            queue.add(sku);
+                            fetchDubboService.sendUrlTask(sku.getWebsite(), sku.getUrl(), cacheSeconds, TaskLevel.LEVEL_3);
+                        } else {
+                            queue.add(sku);
+                            fetchDubboService.sendUrlTask(sku.getWebsite(), sku.getUrl(), cacheSeconds, TaskLevel.LEVEL_5);
+                        }
+
+                        logger.info("send url request succes for " + sku.getWebsite() + " sku id is [" + sku.getId() + "]");
+                    }
                 }
             }
         }
