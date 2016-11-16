f128f65fa7ccada74911d511923195fafa34145f
Joe <zhouwendong006@163.com>
2016-11-16 23:16:16 +0800

    gus it

--- hasoffer.offlinejob/src/main/java/hasoffer/job/bean/order/OrderSAsisJobBean.java
+++ hasoffer.offlinejob/src/main/java/hasoffer/job/bean/order/OrderSAsisJobBean.java
@@ -1,7 +1,8 @@
 package hasoffer.job.bean.order;
 
+import hasoffer.base.model.Website;
+import hasoffer.base.utils.TimeUtils;
 import hasoffer.core.admin.IOrderStatsAnalysisService;
-import org.apache.commons.lang3.time.DateUtils;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.slf4j.Logger;
@@ -9,45 +10,29 @@ import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.quartz.QuartzJobBean;
 
 import javax.annotation.Resource;
-import java.text.ParseException;
 import java.util.Date;
 
-public class OrderSAsisJobBean extends QuartzJobBean {
-    private static Logger logger = LoggerFactory.getLogger(OrderSAsisJobBean.class);
+public class OrderSasisJobBean extends QuartzJobBean {
+    private static Logger logger = LoggerFactory.getLogger(OrderSasisJobBean.class);
 
     @Resource
     IOrderStatsAnalysisService orderStatsAnalysisService;
 
-    public static void main(String[] args) {
-        Date date = new Date();
-        Date targetDate = null;
-        try {
-            targetDate = DateUtils.parseDate("2016-11-21", "yyyy-MM-dd");
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        Long x = (targetDate.getTime() - date.getTime()) / (1000 * 60 * 60);
-        System.out.println(x);
-    }
-
     @Override
     protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
-        try {
+        logger.info("OrderSasisJobBean job start.");
+        Date todayTime = new Date();
 
-            Date date = new Date();
-            Date targetDate = DateUtils.parseDate("2016-11-21", "yyyy-MM-dd");
-            Long x = (targetDate.getTime() - date.getTime()) / (1000 * 60 * 60);
+        int[] days = new int[]{0, 1, 2, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 24, 25, 26, 29, 30, 31, 25, 50, 60};
+        //int[] days = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 10, 13, 15, 18, 20, 25, 30};
+        //int[] days = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,32,33, 34, 35, 36, 40, 45, 50};
 
-            Date start8mTime = DateUtils.parseDate("2016-08-01", "yyyy-MM-dd");
-            Date end8mTime = DateUtils.parseDate("2016-08-31", "yyyy-MM-dd");
-            orderStatsAnalysisService.updateOrderToLow(start8mTime, end8mTime, 592120, x);
-
-            Date start9mTime = DateUtils.parseDate("2016-08-01", "yyyy-MM-dd");
-            Date end9mTime = DateUtils.parseDate("2016-08-31", "yyyy-MM-dd");
-            orderStatsAnalysisService.updateOrderToLow(start9mTime, end9mTime, 692120, x);
-
-        } catch (Exception e) {
-            logger.error("reportOrderStatistic: update order fail. DATE:" + new Date() + ": msg:", e);
+        for (int i : days) {
+            Date day = TimeUtils.addDay(todayTime, -i);
+            orderStatsAnalysisService.updateOrder(Website.SNAPDEAL.name(), day, day);
+            orderStatsAnalysisService.updateOrder(Website.FLIPKART.name(), day, day);
         }
+        logger.info("OrderSasisJobBean job end.");
+
     }
 }
