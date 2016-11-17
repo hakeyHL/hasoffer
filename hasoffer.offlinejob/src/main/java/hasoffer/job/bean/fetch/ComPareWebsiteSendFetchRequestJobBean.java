e105285c6aafb1d1705900bd156503f946cf1d67
wing-SF <18735117232@163.com>
2016-11-17 17:09:37 +0800

    mobile91 data fetch result to new structure

--- hasoffer.offlinejob/src/main/java/hasoffer/job/bean/fetch/ComPareWebsiteSendFetchRequestJobBean.java
+++ hasoffer.offlinejob/src/main/java/hasoffer/job/bean/fetch/ComPareWebsiteSendFetchRequestJobBean.java
@@ -102,7 +102,7 @@ public class ComPareWebsiteSendFetchRequestJobBean extends QuartzJobBean {
                     JSONObject jsonResult = JSONObject.parseObject(postResultString);
                     Integer productCount = jsonResult.getInteger("productCount");
 
-                    JSONArray categoryArray = jsonObject.getJSONArray("categoryFilter");
+                    JSONArray categoryArray = jsonResult.getJSONArray("categoryFilter");
                     for (int k = 0; k < categoryArray.size(); k++) {
 
                         JSONObject jsonCategoryInfo = categoryArray.getJSONObject(k);
