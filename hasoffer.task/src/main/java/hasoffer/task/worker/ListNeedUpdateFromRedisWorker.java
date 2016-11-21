afbec060d1144ea4664d55b69fa9cb9bbf83ad91
wing-SF <18735117232@163.com>
2016-11-21 10:54:44 +0800

    add more log for sku update

--- hasoffer.task/src/main/java/hasoffer/task/worker/ListNeedUpdateFromRedisWorker.java
+++ hasoffer.task/src/main/java/hasoffer/task/worker/ListNeedUpdateFromRedisWorker.java
@@ -91,12 +91,12 @@ public class ListNeedUpdateFromRedisWorker implements Runnable {
 //            }
 
             Object pop = redisListService.pop(UPDATE_WAIT_QUEUE + ymd);
-            if (pop == null) {//如果队列没有数据了，休息30分钟 todo 时间有点长，5分钟合适
+            if (pop == null) {//如果队列没有数据了，休息5分钟
                 try {
-                    System.out.println("sku update pop get null sleep 30 min " + tomorrowDayStart);
-                    TimeUnit.MINUTES.sleep(30);
+                    logger.info("sku update pop get null sleep 30 min " + tomorrowDayStart);
+                    TimeUnit.MINUTES.sleep(5);
                 } catch (InterruptedException e) {
-// todo 记录下异常
+                    logger.info("ListNeedUpdateFromRedisWorker list pop from redis InterruptedException");
                 }
                 continue;
             }
