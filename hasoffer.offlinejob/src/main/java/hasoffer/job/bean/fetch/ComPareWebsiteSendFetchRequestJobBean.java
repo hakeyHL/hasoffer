8bdfcc8e2fab4afec1ff85f790913db20f7a5753
wing-SF <18735117232@163.com>
2016-11-24 09:50:17 +0800

    deal update add delete old and create a new deal function

--- hasoffer.offlinejob/src/main/java/hasoffer/job/bean/fetch/ComPareWebsiteSendFetchRequestJobBean.java
+++ hasoffer.offlinejob/src/main/java/hasoffer/job/bean/fetch/ComPareWebsiteSendFetchRequestJobBean.java
@@ -36,56 +36,56 @@ public class ComPareWebsiteSendFetchRequestJobBean extends QuartzJobBean {
     @Override
     protected void executeInternal(JobExecutionContext jobExecutionContext) throws JobExecutionException {
 //          mobile
-        try {
-            method2();//抓取91mobile数据mobile
-        } catch (HttpFetchException e) {
-            logger.info("fetch mobile category for 91mobile fail");
-        }
+//        try {
+//            method2();//抓取91mobile数据mobile
+//        } catch (HttpFetchException e) {
+//            logger.info("fetch mobile category for 91mobile fail");
+//        }
 
 //          camera
-        try {
-            String tabletProductListUrlPrefix = "http://www.91mobiles.com/template/category_finder/finder_ajax.php?ord=0.8606721456464186&requestType=2&listType=list&listType_v1=&selMobSort=relevance&amount=1000%3B70000&sCatName=phone&price_range_apply=0&search=camera&hidFrmSubFlag=1&page=";
-            String tabletProductListUrlSuffix = "&category=camera&unique_sort=&hdnCategory=camera&user_search=camera";
-            method2(tabletProductListUrlPrefix, tabletProductListUrlSuffix, 3182);
-        } catch (HttpFetchException e) {
-            logger.info("fetch tablet category for 91mobile fail");
-        }
+//        try {
+//            String tabletProductListUrlPrefix = "http://www.91mobiles.com/template/category_finder/finder_ajax.php?ord=0.8606721456464186&requestType=2&listType=list&listType_v1=&selMobSort=relevance&amount=1000%3B70000&sCatName=phone&price_range_apply=0&search=camera&hidFrmSubFlag=1&page=";
+//            String tabletProductListUrlSuffix = "&category=camera&unique_sort=&hdnCategory=camera&user_search=camera";
+//            method2(tabletProductListUrlPrefix, tabletProductListUrlSuffix, 3182);
+//        } catch (HttpFetchException e) {
+//            logger.info("fetch tablet category for 91mobile fail");
+//        }
 
 //          tablet
-        try {
-            String tabletProductListUrlPrefix = "http://www.91mobiles.com/template/category_finder/finder_ajax.php?ord=0.5817923555480047&requestType=1&listType=list&listType_v1=&selMobSort=relevance&amount=1000%3B45000&sCatName=phone&price_range_apply=0&search=tablet&hidFrmSubFlag=1&page=";
-            String tabletProductListUrlSuffix = "&category=tablet&unique_sort=&hdnCategory=tablet&user_search=tablet";
-            method2(tabletProductListUrlPrefix, tabletProductListUrlSuffix, 57);
-        } catch (HttpFetchException e) {
-            logger.info("fetch tablet category for 91mobile fail");
-        }
+//        try {
+//            String tabletProductListUrlPrefix = "http://www.91mobiles.com/template/category_finder/finder_ajax.php?ord=0.5817923555480047&requestType=1&listType=list&listType_v1=&selMobSort=relevance&amount=1000%3B45000&sCatName=phone&price_range_apply=0&search=tablet&hidFrmSubFlag=1&page=";
+//            String tabletProductListUrlSuffix = "&category=tablet&unique_sort=&hdnCategory=tablet&user_search=tablet";
+//            method2(tabletProductListUrlPrefix, tabletProductListUrlSuffix, 57);
+//        } catch (HttpFetchException e) {
+//            logger.info("fetch tablet category for 91mobile fail");
+//        }
 
 //          tv
-        try {
-            String tabletProductListUrlPrefix = "http://www.91mobiles.com/template/category_finder/finder_ajax.php?ord=0.31939880350088856&requestType=2&listType=list&listType_v1=&selMobSort=relevance&amount=1000%3B70000&sCatName=phone&price_range_apply=0&search=television&hidFrmSubFlag=1&page=";
-            String tabletProductListUrlSuffix = "&category=television&unique_sort=&hdnCategory=television&user_search=television";
-            method2(tabletProductListUrlPrefix, tabletProductListUrlSuffix, 3100);
-        } catch (HttpFetchException e) {
-            logger.info("fetch tablet category for 91mobile fail");
-        }
+//        try {
+//            String tabletProductListUrlPrefix = "http://www.91mobiles.com/template/category_finder/finder_ajax.php?ord=0.31939880350088856&requestType=2&listType=list&listType_v1=&selMobSort=relevance&amount=1000%3B70000&sCatName=phone&price_range_apply=0&search=television&hidFrmSubFlag=1&page=";
+//            String tabletProductListUrlSuffix = "&category=television&unique_sort=&hdnCategory=television&user_search=television";
+//            method2(tabletProductListUrlPrefix, tabletProductListUrlSuffix, 3100);
+//        } catch (HttpFetchException e) {
+//            logger.info("fetch tablet category for 91mobile fail");
+//        }
 
 //          memory card
         try {
-            String memoryCardRequestRaw = "listType=list&selMobSort=views&amount=100%3B10000&sCatName=memorycard&market_status%5B%5D=ava_stores&hidFrmSubFlag=1&category=memorycard&hdnCategory=memorycard&hdnPageType=list&resFormat=list&device_category=m&page=1";
+            String memoryCardRequestRaw = "listType=list&selMobSort=views&amount=100%3B10000&sCatName=memorycard&market_status%5B%5D=ava_stores&hidFrmSubFlag=1&category=memorycard&hdnCategory=memorycard&hdnPageType=list&resFormat=list&device_category=m&page=";
             method3(memoryCardRequestRaw, 205);
         } catch (Exception e) {
             logger.info("fetch post memory card category for 91mobile fail");
         }
 //          power bank
         try {
-            String powerBankCardRequestRaw = "listType=list&selMobSort=views&amount=200%3B10000&sCatName=powerbank&market_status%5B%5D=ava_stores&q=&hidFrmSubFlag=1&category=powerbank&unique_sort=&hdnCategory=powerbank&hdnPageType=list&resFormat=list&device_category=m&page=1";
+            String powerBankCardRequestRaw = "listType=list&selMobSort=views&amount=200%3B10000&sCatName=powerbank&market_status%5B%5D=ava_stores&q=&hidFrmSubFlag=1&category=powerbank&unique_sort=&hdnCategory=powerbank&hdnPageType=list&resFormat=list&device_category=m&page=";
             method3(powerBankCardRequestRaw, 270);
         } catch (Exception e) {
             logger.info("fetch post power bank category for 91mobile fail");
         }
 //          smart watch
         try {
-            String smartWatchRequestRaw = "listType=list&selMobSort=views&amount=1000%3B25000&sCatName=smartwatch&market_status%5B%5D=all&q=&hidFrmSubFlag=1&category=smartwatch&unique_sort=&hdnCategory=smartwatch&hdnPageType=list&resFormat=list&device_category=m&page=1";
+            String smartWatchRequestRaw = "listType=list&selMobSort=views&amount=1000%3B25000&sCatName=smartwatch&market_status%5B%5D=all&q=&hidFrmSubFlag=1&category=smartwatch&unique_sort=&hdnCategory=smartwatch&hdnPageType=list&resFormat=list&device_category=m&page=";
             method3(smartWatchRequestRaw, 102909);
         } catch (Exception e) {
             logger.info("fetch post smart watch category for 91mobile fail");
@@ -112,13 +112,12 @@ public class ComPareWebsiteSendFetchRequestJobBean extends QuartzJobBean {
         for (int i = 1; i <= totalPage; i++) {
 
             if (i > 1) {
-                requestRaw += i;
-                responseHtml = HtmlHelper.doPostJsonWithHeader(url, requestRaw + 1, headerMap);
+                responseHtml = HtmlHelper.doPostJsonWithHeader(url, requestRaw + i, headerMap);
             }
 
             String[] subStr2 = responseHtml.split("hover_blue_link name\" href=\"");
             for (int j = 1; j < subStr2.length; j++) {
-                String productUrl = WEBSITE_91MOBILE_URL_PREFIEX + subStr2[i].substring(0, subStr2[i].indexOf('"'));
+                String productUrl = WEBSITE_91MOBILE_URL_PREFIEX + subStr2[j].substring(0, subStr2[j].indexOf('"'));
                 fetchDubboService.sendCompareWebsiteFetchTask(Website.MOBILE91, productUrl, TaskLevel.LEVEL_1, categoryId);
                 requestSendNumber++;
                 logger.info("hava send " + requestSendNumber + " request ");
