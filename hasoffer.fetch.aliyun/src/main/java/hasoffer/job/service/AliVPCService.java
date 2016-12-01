e23a9fabace9d5d503b6c341dfd68d10fd40fa5d
Joe <zhouwendong006@163.com>
2016-12-01 17:58:56 +0800

    修改mysql链接。

--- hasoffer.fetch.aliyun/src/main/java/hasoffer/job/service/AliVPCService.java
+++ hasoffer.fetch.aliyun/src/main/java/hasoffer/job/service/AliVPCService.java
@@ -1,6 +1,7 @@
 package hasoffer.job.service;
 
 import hasoffer.aliyun.api.model.EipAddressModel;
+import hasoffer.data.redis.IRedisMapService;
 import hasoffer.job.dao.AliVPCDAO;
 import hasoffer.job.dmo.AliVPC;
 import org.springframework.stereotype.Service;
@@ -14,11 +15,18 @@ public class AliVPCService {
     @Resource
     private AliVPCDAO aliVPCDAO;
 
+    @Resource
+    private IRedisMapService<String, String> mapService;
+
     public List<AliVPC> queryAllVPCList() {
         return aliVPCDAO.queryAllVPCList();
     }
 
     public void updateEipInfo(String ecsInstance, EipAddressModel eipAddressModel) {
         aliVPCDAO.updateEipInfo(ecsInstance, eipAddressModel);
+    }
+
+    public void updateVpcStatus(AliVPC vpc, boolean status) {
+        mapService.putMap("ALI-VPC-STATUS", vpc.getPrivateIpAddress(), status ? "Y" : "N");
     }
 }
