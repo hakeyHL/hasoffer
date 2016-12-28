d592ea63f9ff8ed0f32ad2f7a9b55bbcba71f7d0
Joe <zhouwendong006@163.com>
2016-12-28 20:50:44 +0800

    IP定时更新

--- hasoffer.fetch.aliyun/src/main/java/hasoffer/job/bean/MatchStateJobBean.java
+++ hasoffer.fetch.aliyun/src/main/java/hasoffer/job/bean/MatchStateJobBean.java
@@ -1,11 +1,7 @@
 package hasoffer.job.bean;
 
-import hasoffer.aliyun.enums.WebSite;
 import hasoffer.data.redis.IRedisService;
-import hasoffer.state.dmo.MatchStateDMO;
 import hasoffer.state.service.MatchStateService;
-import hasoffer.state.service.UpdateStateService;
-import org.apache.commons.lang3.time.DateFormatUtils;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.slf4j.Logger;
@@ -13,8 +9,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.quartz.QuartzJobBean;
 
 import javax.annotation.Resource;
-import java.util.Date;
-import java.util.List;
 
 public class MatchStateJobBean extends QuartzJobBean {
 
@@ -34,38 +28,37 @@ public class MatchStateJobBean extends QuartzJobBean {
     }
 
     private void stateTask() {
-        for (WebSite website : WebSite.values()) {
-            MatchStateDMO updateStateDMO = new MatchStateDMO();
-            int pushNum = 0;
-            int finishNum = 0;
-            int exceptionNum = 0;
-            int stopNum = 0;
-            String ymd = DateFormatUtils.format(new Date(), "yyyyMMdd");
-
-            String finishKey = "MATCH_POP_NUM_" + website.name() + "_" + UpdateStateService.TaskStatus.FINISH + "_" + ymd;
-            String finishNumStr = redisService.get(finishKey, -1);
-            logger.info(finishKey + " value: {}", finishNumStr);
-            finishNum = Integer.valueOf(finishNumStr == null ? "0" : finishNumStr) + finishNum;
-
-            String exceptionKey = "MATCH_POP_NUM_" + website.name() + "_" + UpdateStateService.TaskStatus.EXCEPTION + "_" + ymd;
-            String exceptionNumStr = redisService.get(exceptionKey, -1);
-            logger.info(exceptionKey + " value: {}", exceptionNumStr);
-            exceptionNum = Integer.valueOf(exceptionNumStr == null ? "0" : exceptionNumStr) + exceptionNum;
-
-
-            String updateStr = DateFormatUtils.format(new Date(), "yyyy-MM-dd");
-            updateStateDMO.setWebSite(website.name());
-            updateStateDMO.setPushNum(pushNum);
-            updateStateDMO.setExceptionNum(exceptionNum);
-            updateStateDMO.setFinishNum(finishNum);
-            updateStateDMO.setUpdateDate(updateStr);
-            updateStateDMO.setLogTime(new Date());
-            List<MatchStateDMO> updateStateDMOs = matchStateService.selectByDate(updateStr);
-            if (updateStateDMOs.size() == 0) {
-                matchStateService.insert(updateStateDMO);
-            } else {
-                matchStateService.update(updateStateDMO);
-            }
-        }
+        //for (WebSite website : WebSite.values()) {
+        //    MatchStateDMO updateStateDMO = new MatchStateDMO();
+        //    int pushNum = 0;
+        //    int finishNum = 0;
+        //    int exceptionNum = 0;
+        //    String ymd = DateFormatUtils.format(new Date(), "yyyyMMdd");
+        //
+        //    String finishKey = "MATCH_POP_NUM_" + website.name() + "_" + UpdateStateService.TaskStatus.FINISH + "_" + ymd;
+        //    String finishNumStr = redisService.get(finishKey, -1);
+        //    logger.info(finishKey + " value: {}", finishNumStr);
+        //    finishNum = Integer.valueOf(finishNumStr == null ? "0" : finishNumStr) + finishNum;
+        //
+        //    String exceptionKey = "MATCH_POP_NUM_" + website.name() + "_" + UpdateStateService.TaskStatus.EXCEPTION + "_" + ymd;
+        //    String exceptionNumStr = redisService.get(exceptionKey, -1);
+        //    logger.info(exceptionKey + " value: {}", exceptionNumStr);
+        //    exceptionNum = Integer.valueOf(exceptionNumStr == null ? "0" : exceptionNumStr) + exceptionNum;
+        //
+        //
+        //    String updateStr = DateFormatUtils.format(new Date(), "yyyy-MM-dd");
+        //    updateStateDMO.setWebSite(website.name());
+        //    updateStateDMO.setPushNum(pushNum);
+        //    updateStateDMO.setExceptionNum(exceptionNum);
+        //    updateStateDMO.setFinishNum(finishNum);
+        //    updateStateDMO.setUpdateDate(updateStr);
+        //    updateStateDMO.setLogTime(new Date());
+        //    List<MatchStateDMO> updateStateDMOs = matchStateService.selectByDate(updateStr);
+        //    if (updateStateDMOs.size() == 0) {
+        //        matchStateService.insert(updateStateDMO);
+        //    } else {
+        //        matchStateService.update(updateStateDMO);
+        //    }
+        //}
     }
 }
