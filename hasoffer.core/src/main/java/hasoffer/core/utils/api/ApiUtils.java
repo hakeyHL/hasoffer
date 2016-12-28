db75cb9e67c6a9f40745d04b763ae89396e28548
huli <hakeyhl@163.com>
2016-12-28 12:11:27 +0800

    1. order split date .

--- hasoffer.core/src/main/java/hasoffer/core/utils/api/ApiUtils.java
+++ hasoffer.core/src/main/java/hasoffer/core/utils/api/ApiUtils.java
@@ -39,6 +39,8 @@ import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 import java.lang.reflect.Field;
 import java.math.BigDecimal;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -870,11 +872,22 @@ public class ApiUtils {
      * @param data
      */
     public void calculateHasofferCoin(List<UrmUser> users, BackDetailVo data) {
+        //订单分隔时间 2016年12月28日 12:00:00
+        String splitTime = "2016/12/28 12:00:00";
+        Date splitDate;
+        try {
+            splitDate = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").parse(splitTime);
+        } catch (ParseException e) {
+            System.out.println("transfer time failed .");
+            return;
+        }
         List<OrderVo> transcations = new ArrayList<OrderVo>();
         BigDecimal pendingCoins = BigDecimal.ZERO;
-        BigDecimal verifiedCoins = BigDecimal.ZERO;
+        BigDecimal multipliedVerifiedCoins = BigDecimal.ZERO;
+        BigDecimal addedVerifiedCoins = BigDecimal.ZERO;
         for (UrmUser user : users) {
             List<OrderStatsAnalysisPO> orders = appService.getBackDetails(user.getId().toString());
+
             for (OrderStatsAnalysisPO orderStatsAnalysisPO : orders) {
                 if (orderStatsAnalysisPO.getWebSite().equals(Website.FLIPKART.name())) {
                     OrderVo orderVo = new OrderVo();
@@ -895,7 +908,11 @@ public class ApiUtils {
                             }
                         }
                         if (orderStatsAnalysisPO.getOrderStatus().equals("approved")) {
-                            verifiedCoins = verifiedCoins.add(tempPrice);
+                            if (orderStatsAnalysisPO.getOrderTime().compareTo(splitDate) == -1) {
+                                multipliedVerifiedCoins = multipliedVerifiedCoins.add(tempPrice);
+                            } else {
+                                addedVerifiedCoins = addedVerifiedCoins.add(tempPrice);
+                            }
                         }
                     }
 
@@ -905,8 +922,8 @@ public class ApiUtils {
         //待定的
         data.setPendingCoins(pendingCoins.divide(BigDecimal.ONE, 0, BigDecimal.ROUND_HALF_UP));
         //可以使用的
-//        verifiedCoins = verifiedCoins.multiply(BigDecimal.TEN);
-        data.setVerifiedCoins(verifiedCoins.divide(BigDecimal.ONE, 0, BigDecimal.ROUND_HALF_UP));
+        multipliedVerifiedCoins = multipliedVerifiedCoins.multiply(BigDecimal.TEN).add(addedVerifiedCoins);
+        data.setVerifiedCoins(multipliedVerifiedCoins.divide(BigDecimal.ONE, 0, BigDecimal.ROUND_HALF_UP));
         data.setTranscations(transcations);
     }
 }
