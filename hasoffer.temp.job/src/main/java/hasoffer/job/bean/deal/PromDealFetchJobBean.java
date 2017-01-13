a2a6436b36a780e7d47124a4f38e8b1f3d948497
Joe <zhouwendong006@163.com>
2017-01-13 13:42:07 +0800

    默认两天后失效

--- hasoffer.temp.job/src/main/java/hasoffer/job/bean/deal/PromDealFetchJobBean.java
+++ hasoffer.temp.job/src/main/java/hasoffer/job/bean/deal/PromDealFetchJobBean.java
@@ -210,7 +210,7 @@ public class PromDealFetchJobBean extends QuartzJobBean {
                 mexicoAppDeal.setLinkUrl(url);
                 mexicoAppDeal.setDescription(descriptionWithOutHtml);
                 mexicoAppDeal.setCreateTime(TimeUtils.nowDate());
-                mexicoAppDeal.setCreateTime(TimeUtils.add(TimeUtils.nowDate(), TimeUtils.MILLISECONDS_OF_1_DAY * 2));
+                mexicoAppDeal.setExpireTime(TimeUtils.add(TimeUtils.nowDate(), TimeUtils.MILLISECONDS_OF_1_DAY * 2));
                 logger.info("insert into appDeal:{}", mexicoAppDeal.toString());
                 dealService.createAppDealByPriceOff(mexicoAppDeal);
             }
