0b0f669e23e3872f5ccded386f82af0a1a39b55f
wuning <wuning@hasoffer.com>
2017-02-13 12:14:18 +0800

    appdeal add  code

--- hasoffer.job/src/main/java/hasoffer/job/bean/deal/DealSiteGetDealWoker.java
+++ hasoffer.job/src/main/java/hasoffer/job/bean/deal/DealSiteGetDealWoker.java
@@ -7,16 +7,19 @@ import hasoffer.base.utils.TimeUtils;
 import hasoffer.core.admin.IDealService;
 import hasoffer.core.persistence.enums.AppdealSource;
 import hasoffer.core.persistence.po.app.AppDeal;
+import hasoffer.core.utils.Httphelper;
 import hasoffer.core.utils.ImageUtil;
 import hasoffer.dubbo.api.fetch.service.IFetchDubboService;
 import hasoffer.fetch.helper.WebsiteHelper;
 import hasoffer.spider.model.FetchDealResult;
 import hasoffer.spider.model.FetchedDealInfo;
+import jodd.io.FileUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -132,17 +135,25 @@ public class DealSiteGetDealWoker implements Runnable {
             appdeal.setPriceDescription(fetchedDealInfo.getPriceString());
         }
 
+
         String dealPath = "";
         String dealBigPath = "";
         String dealSmallPath = "";
 
         try {
-            File imageFile = ImageUtil.downloadImage(fetchedDealInfo.getImageUrl());
+            String response = Httphelper.doGetWithHeaer("https://assets.mysmartprice.com/t_d-desktop-single,f_auto/d/242356.jpg", new HashMap());
+
+            byte[] bodyBytes = response.getBytes();
+
+            File imageFile = FileUtil.createTempFile("mysmartprice", ".jpg", null);
+
+            FileUtil.writeBytes(imageFile, bodyBytes);
 
             dealPath = ImageUtil.uploadImage(imageFile);
             dealBigPath = ImageUtil.uploadImage(imageFile, 316, 180);
             dealSmallPath = ImageUtil.uploadImage(imageFile, 180, 180);
         } catch (Exception e) {
+            e.printStackTrace();
             logger.info("check get priceoff deal image download error");
             return null;
         }
