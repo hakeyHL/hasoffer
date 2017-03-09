07534bb357d85fef020317a1e3ccc093ec194c7e
huli <hakeyhl@163.com>
2017-03-09 18:02:33 +0800

    1. move hjk to app callback.

--- hasoffer.api/src/main/java/hasoffer/api/helper/website/ApiSnapDealHelper.java
+++ hasoffer.api/src/main/java/hasoffer/api/helper/website/ApiSnapDealHelper.java
@@ -1,14 +1,8 @@
 package hasoffer.api.helper.website;
 
-import hasoffer.base.model.HttpResponseModel;
-import hasoffer.base.utils.HtmlUtils;
-import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import us.codecraft.webmagic.selector.Html;
-import us.codecraft.webmagic.selector.Selectable;
 
-import java.net.URLEncoder;
 import java.util.List;
 
 /**
@@ -20,7 +14,7 @@ public class ApiSnapDealHelper {
 
     private static List getFlipKartSkuListByTitleSearch(String title) {
         String baseUrl = "https://www.snapdeal.com/search?keyword=%s&sort=rlvncy";
-        Html html = null;
+      /*  Html html = null;
         if (StringUtils.isNotEmpty(title)) {
             try {
                 title = URLEncoder.encode(title, "utf-8");
@@ -39,7 +33,7 @@ public class ApiSnapDealHelper {
             return null;
         }
         Selectable xpath = html.xpath("/html/body/div[@id='content_wrapper']/div[@class='col-xs-24 reset-padding marT22']/div[@class='col-xs-19 reset-padding ']/div[@class='comp comp-right-wrapper ref-freeze-reference-point clear']/div[@id='products']/section[@class='js-section clearfix dp-widget dp-fired'][1]/div[@id='663413326062']/div[@class='product-tuple-image ']/a[@class='dp-widget-link']/@href");
-        System.out.println(xpath);
+        System.out.println(xpath);*/
         return null;
     }
 
